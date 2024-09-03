40353684f3374f81324eb254d6738575966d8b7d
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-04 02:29:36 +0530

    Strings

--- Lecture/Java Script/JavaScript 1 Introduction 1/JavaScript 10 Strings.js
+++ Lecture/Java Script/JavaScript 1 Introduction 1/JavaScript 10 Strings.js
@@ -3,4 +3,10 @@
 Available data types in js:
     number , string , boolean , bigint , null , undefined , object , symbol
 
+    String represents textual data
+
 */
+let singleQuoteString='Hello World';
+let doubleQuoteString="Hello World";
+let backtickString=`Hello World`;
+console.log(singleQuoteString,doubleQuoteString,backtickString)
