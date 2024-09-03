293022412b6eda51053be18eb5400151ed639bfc
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-04 02:18:55 +0530

    String

---
+++ Lecture/Java Script/JavaScript 1 Introduction 1/JavaScript 10 Strings.js
@@ -0,0 +1,6 @@
+/* Data type represent the different kind of values we can store in the variable 
+
+Available data types in js:
+    number , string , boolean , bigint , null , undefined , object , symbol
+
+*/
---
+++ Lecture/Java Script/JavaScript 1 Introduction 1/JavaScript 9 Variables fact.js
@@ -0,0 +1,10 @@
+let productName="Samsun S23 ultra",productPrice=113000;
+let x="variable x"; // let and var are re-assignable variables means we can later update the values 
+var y="variable y";
+
+const z="variable z"; // const variable is not updatable
+console.log(productName,productPrice);
+productName="i phone pro"
+
+
+console.log("After updating the product name : ",productName);
