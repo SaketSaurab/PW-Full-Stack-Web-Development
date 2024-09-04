bf40164c6e30a937175214d08eb0cddfdc461fee
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-04 09:29:08 +0530

    Null & Undefined

---
+++ Lecture/Java Script/JavaScript 1 Introduction 1/JavaScript 12 Null Undefined.js
@@ -0,0 +1,11 @@
+/*
+Null and Undefined are used to show the sense of emptiness
+null : it is declared and defined but it have no values
+undefined: it is declaired but not defined
+*/
+let x=null;
+let y=undefined;  //method to declare undefined
+let z; //2nd method to declare undefined
+// use let and var to define undefined but not const
+console.log(x,y);
+console.log("2nd method to define undefined :",z);
