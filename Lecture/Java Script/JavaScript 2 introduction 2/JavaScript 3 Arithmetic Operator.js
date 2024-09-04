dda555cfdc9c6487bda6d3d3de98b0bd89599fcc
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-05 03:01:03 +0530

    Arithmetic Operators

---
+++ Lecture/Java Script/JavaScript 2 introduction 2/JavaScript 3 Arithmetic Operator.js
@@ -0,0 +1,14 @@
+/*
++ additon 
+- subtraction
+* mulitplication
+/ division
+** exponent / power : 2**3 =2^3
+% module operation : it gives remainder
+
+++ increament by 1 operator
+-- decreament by 1 operator
+
+*/
+
+console.log(2**3);
