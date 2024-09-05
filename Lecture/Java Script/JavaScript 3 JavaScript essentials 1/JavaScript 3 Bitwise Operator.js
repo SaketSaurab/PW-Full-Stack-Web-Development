231102fd3136c1cb90b6a578a689a964698c34f6
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-05 23:58:40 +0530

    Bitwise operator

---
+++ Lecture/Java Script/JavaScript 3 JavaScript essentials 1/JavaScript 3 Bitwise Operator.js
@@ -0,0 +1,18 @@
+// bitwise operators
+
+//Bitwise and (&)
+console.log("Bitwise & : (5&6) =",(5 & 6));
+
+//bitwise or (|)
+console.log("Bitwise | : (4|6) =",(4 | 6));
+
+//Bitwise xor ^ 
+
+console.log("Xor (5^6)",5^6);
+
+//left shift (<<)
+console.log("left shift (<<):",5<<3);
+
+
+//right shift (>>)
+console.log("right shift (>>):",15>>1);
