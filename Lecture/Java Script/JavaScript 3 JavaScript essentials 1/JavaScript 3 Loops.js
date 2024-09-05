63de5c5cd4fc59569ae53eb970f932c8c150dcbd
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-06 00:18:15 +0530

    Loops

---
+++ Lecture/Java Script/JavaScript 3 JavaScript essentials 1/JavaScript 3 Loops.js
@@ -0,0 +1,22 @@
+//while loop
+console.log("while loop");
+let i=1;
+while(i<=10){
+    console.log(i);
+    i+=1;
+}
+
+// for loop
+console.log("for loop");
+for(k=1;k<=10;k++){
+    console.log(k);
+}
+
+//do while loop
+console.log("do while loop");
+let a=12;
+do{
+    console.log(a);
+    a+=1;
+}
+while(a<=20);
