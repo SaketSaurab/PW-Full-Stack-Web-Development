eb94848383e78ba4b39aa0e83a75e1226459d424
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-06 18:24:44 +0530

    Introduction to Functions

---
+++ Lecture/Java Script/JavaScript 3 JavaScript essentials 1/JavaScript 4 Functions.js
@@ -0,0 +1,24 @@
+// usnig the below syntax we can define our black box
+function greeetingToEveryone(){
+    console.log("Hello");
+    console.log("Good Morning");
+}
+// calling our function
+greeetingToEveryone();
+
+// function 2 = Math.sqrt
+
+console.log(Math.sqrt(100));
+
+
+//even or oddd function
+
+function isEvenOrOdd(x){
+    if(x%2==0){
+        console.log("even number");
+    }
+    else{
+        console.log("Odd number");
+    }
+}
+isEvenOrOdd(3);
