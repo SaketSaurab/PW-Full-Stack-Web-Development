f40599f395d86dccc9409b0959dcca3ac2372442
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-07 02:54:43 +0530

    Function test

--- Lecture/Java Script/JavaScript 4 JavaScript Essentials 2/JavaScript Essentials 2 1 Function test.js
+++ Lecture/Java Script/JavaScript 4 JavaScript Essentials 2/JavaScript Essentials 2 1 Function test.js
@@ -5,10 +5,18 @@ function f(x){
     x*=4;
     return x*7;
 }
-// if we do not return manually then javascript will return undefined
+
 function g(y){
     console.log(y*3);
     let z=f(y);
     return z;
 }
 let ans=g(2);
+
+// if we do not return manually then javascript will return undefined
+function test(){
+    let a=10;
+    a+=4;
+}
+let ans2=test();
+console.log(ans2);
