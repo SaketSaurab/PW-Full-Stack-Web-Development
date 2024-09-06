2bfddd72123f16646d30a5f0b970b96611158864
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-07 05:16:48 +0530

    Introduction to Array

---
+++ Lecture/Java Script/JavaScript 4 JavaScript Essentials 2/JavaScript Essentials 2  3 Introduction to array.js
@@ -0,0 +1,8 @@
+// Arrays are a special object in JavaScript , that can store 1 or more than 1 value of different types together.
+//in memory, array are  created in 1-D space
+// these are 1-d cross section in memory
+// arrays in javascript  is heterogeneous 
+let arr1=[]; // empty array
+console.log(arr1);
+let arr2 =["hello", 1,35,true, null ,"world"];
+console.log(arr2);
