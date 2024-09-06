3f98ea552102d3d45cdf6892dcc2b4cd95df1cf5
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-07 03:35:25 +0530

    Date function

---
+++ Lecture/Java Script/JavaScript 4 JavaScript Essentials 2/JavaScript Essentials 2 1 date function.js
@@ -0,0 +1,9 @@
+const d=new Date();
+console.log(d);
+
+console.log("d.getTime :",d.getTime());
+console.log("d.getFullYear :",d.getFullYear());
+console.log("d.getMonth :",d.getMonth());
+console.log("d.getDay :",d.getDay());
+console.log("d.getMinutes :",d.getMinutes());
+console.log("d.getMilliseconds :",d.getMilliseconds());
