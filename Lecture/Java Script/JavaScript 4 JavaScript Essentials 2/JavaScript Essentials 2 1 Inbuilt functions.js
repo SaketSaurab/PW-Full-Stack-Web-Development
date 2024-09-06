cd742b7cbd65e9e878df0e470d3a43a4fdd40150
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-07 03:25:20 +0530

    Inbuild functions

---
+++ Lecture/Java Script/JavaScript 4 JavaScript Essentials 2/JavaScript Essentials 2 1 Inbuilt functions.js
@@ -0,0 +1,38 @@
+console.log("Math.abs function");
+
+//this function will give us absolute values
+console.log(Math.abs(5));
+console.log(Math.abs(-5));
+
+// math is a object in javascript
+// console.log(Math);
+console.log("Math.floor");
+// it will give the smaller number 
+console.log(Math.floor(8.2));
+
+console.log("Math.ceil");
+// it will give bigger number than the input number
+console.log(Math.ceil(8.7));
+
+console.log("Math.round");
+// it will give the floor value if point number is less than five and give ceil number if point number is more than 5
+console.log(Math.round(8.4));
+console.log(Math.round(8.6));
+
+console.log("Math.min");
+// it will give the smallest number among all the numbers
+console.log(Math.min(3,53,52,0,-5,-3,-9));
+
+console.log("Math.max");
+// it will give the largest number among all the numbers
+console.log(Math.max(3,53,52,0,-5,-3,-9));
+
+console.log("Math.sqrt");
+// it will give the sqare root of the numbers
+console.log(Math.sqrt(57));
+
+
+
+console.log("Math.random");
+// it will generate random number between 0 and 1
+console.log(Math.random());
