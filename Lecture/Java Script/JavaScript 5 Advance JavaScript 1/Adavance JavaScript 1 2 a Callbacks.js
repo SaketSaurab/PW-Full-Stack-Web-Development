3d3775fe2f50c83036aba432561d434ffff3e1be
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-08 03:52:01 +0530

    Call back

---
+++ Lecture/Java Script/JavaScript 5 Advance JavaScript 1/Adavance JavaScript 1 2 a Callbacks.js
@@ -0,0 +1,33 @@
+// call back function
+// function h(x,fn){
+//     // h is higher order function
+//     // fn is call back
+//     console.log(x*x);
+//     fn();
+// }
+// h(10,function(){
+//     console.log("done with call back")
+// })
+
+// // second method
+// console.log("Second method");
+
+// function h(x,fn){
+//     // h is higher order function
+//     // fn is call back
+//     console.log(x*x);
+//     fn(x*x);
+// }
+// h(10,exec);
+
+// function exec(n){
+//     console.log("Squared value is ",n);
+// }
+////Asynchronous piece of code////
+console.log("Start");
+setTimeout(function(){
+    console.log("Timer done");
+},3000);
+
+console.log("end");
+
