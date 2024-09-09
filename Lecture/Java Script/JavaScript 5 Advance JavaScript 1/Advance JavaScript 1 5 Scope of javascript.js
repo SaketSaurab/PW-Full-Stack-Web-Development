1d78b9f2ba6b6b9c7f0d21947109f8924abec599
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-09 12:28:23 +0530

    Introduction to JavaScript scope

---
+++ Lecture/Java Script/JavaScript 5 Advance JavaScript 1/Advance JavaScript 1 5 Scope of javascript.js
@@ -0,0 +1,15 @@
+/* JavaScript pillars
+    a.Scopes and clouser
+    b.coercion and types
+    c.objects and classes
+    d.async programming
+*/
+/*
+Java script is a hybrid language
+
+javascript combines the process of compilation and interpretation
+every javascript code is executed in 2 phase
+Phase 1. compilation and scope resolution
+Phase 2. Interpretation or execution phase
+*/
+
