f06b82d336155dcbb92694daef8c0108769574fe
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-10 01:13:52 +0530

    types of scope

--- Lecture/Java Script/JavaScript 6 Advance JavaScript 2/Advance javascript 2 1 Types of Scope.js
+++ Lecture/Java Script/JavaScript 6 Advance JavaScript 2/Advance javascript 2 1 Types of Scope.js
@@ -39,13 +39,13 @@ gun();
 console.log("the value of y is ",y); // y is not visible here
 */
 
-// block scope
-console.log("Block scope");
-{
-    let z=10;
-    console.log(z);
-}
-console.log(z);  // z is not visible here
+// // block scope
+// console.log("Block scope");
+// {
+//     let z=10;
+//     console.log(z);
+// }
+// console.log(z);  // z is not visible here
 
 /* Ways to declare variables : 
 var :  use to declare function scope and global scope variables
@@ -53,3 +53,35 @@ let : helps us to initialize block scope variables
 const: const also helps us to initialize block scope variables
 
 */
+
+/*
+Any variable is used in two ways
+RHS ->when we consume the variable
+LHS ->when we assign or declare the value
+
+
+*/
+
+console.log("LHS and RHS of the variable");
+
+var a=99; //LHS - we are assigning the value 
+console.log(a); //RHS- we are consuming the value of  the variable
+
+
+// Lexical scoping or lexical parsing
+// JS does scope resolution using lexical scoping mechanism. also called static scoping
+// 
+
+var teacher="Sanket singh";
+function fun3(){
+    var teacher="sarthak";
+    console.log("hello",teacher);
+}
+function gun2(){
+    var student="aman";
+    console.log(student,teacher);
+}
+fun3();
+gun2();
+
+// "use strict";  it prevents the creations of auto global variables
