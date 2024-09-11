fc8fa7d44548a930eb00a00cbb22d8a391c10dbd
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-12 00:21:13 +0530

    OOPS 1

--- Lecture/Java Script/JavaScript 7 OOPS/JavaScript OOPS 1.js
+++ Lecture/Java Script/JavaScript 7 OOPS/JavaScript OOPS 1.js
@@ -2,3 +2,18 @@
 // Classes are the blueprint
 //objects :using classes the final entity that we will develop is called as objects
 //
+
+class Product{
+    addToCart(){
+        console.log("Product added to the cart ");
+    }
+    removeFromCart(){
+        console.log("Product removed from cart");
+    }
+    displayProduct(){
+        console.log("Product displayed");
+    }
+    buyProduct(){
+        console.log("Product bought");
+    }
+}
