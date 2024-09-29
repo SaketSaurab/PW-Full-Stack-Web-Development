444450d24a582a8e6629f395882168c8aa23ccdc
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-09-29 23:09:57 +0530

    Intro to react 2A

---
+++ Lecture/React/React 1 Introduction to React/Intro to react 2.js
@@ -0,0 +1,11 @@
+/* Component driven architchture
+Cobination of mutliple react elements create components.
+React element is an object that describes the properties of an actual dom node which will be crated by react
+
+eg. {
+    type: 'button'
+    ..
+    ..
+    }
+
+*/
