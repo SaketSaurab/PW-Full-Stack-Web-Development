d92f16121fe4fd287d8865975c1ba0b49fee4fed
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-10-28 17:35:35 +0530

    operators and its types

--- Practice/JavaScript/hello world.js
+++ Practice/JavaScript/hello world.js
@@ -3,3 +3,45 @@ console.log('single quote');
 console.log("double quote");
 console.log("number:",10);
 console.log("decimal number :", 3.79);
+console.log("Big int = (2^53 -1) to -(2^53-1)",4654478484564687684645478958n);
+console.log("boolean: ", true,false);
+// null means : nothing or empty value
+// undefined means : variable is declared but not assigned any value
+//single line comments
+/*
+this is
+multi line
+comment
+*/
+// operators
+// assignment operators
+
+var name="pw skills";
+console.log(name);
+// arithamtic operators
+var num1=23;
+var num2=14;
+var sum=num1+num2;
+console.log("sum of :",num1,"and ",num2,"is ",sum);
+
+var multiply=num1*num2;
+console.log("multiplication of :",num1,"and ",num2,"is ",multiply);
+
+var division=num1/num2;
+
+console.log("division  of :",num1,"and ",num2,"is ",division);
+// exponential 
+
+console.log("3^4=",3**4);
+
+//increment operator
+var inc1=5;
+var inc2=7;
+console.log("inc1=5 , ",inc1++);
+console.log(inc1);
+console.log("inc2 =7 ,",++inc2);
+
+var numm1=10;
+var numm2="10";
+console.log("equal",numm1==numm2);
+console.log("strict equal",numm1===numm2);
