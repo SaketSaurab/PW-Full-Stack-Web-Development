a54752756c3ffb44efc9fc825b8ea0735776967a
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-05 00:50:08 +0530

    anonymous function

--- Lecture/Java Script/JavaScript 5 Advance JavaScript 1/Adavance JavaScript 1 1 Anonymous Function.js
+++ Lecture/Java Script/JavaScript 5 Advance JavaScript 1/Adavance JavaScript 1 1 Anonymous Function.js
@@ -3,7 +3,10 @@ let x =function(){      // anonymous function declaration
     // debugging of anonymous function is very hard
     console.log("hello");
 }
+
+
 x();
+
 
 // Immediately invoke functions
 ( function exec(){            // 
