7cb502b126b37dc6e837d234d5bad7277ae1ee3c
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-06 23:07:55 +0530

    achiving immutability

--- Practice/JavaScript/introduction to object.js
+++ Practice/JavaScript/introduction to object.js
@@ -9,4 +9,20 @@ let Product={
 console.log(Product);
 
 //Mutability and immutability in JS
-//variable initialize with const can not be updated 
+//variable initialize with const can not be updated and it become immutable
+const a=10;
+// a=5;     this is not allowed
+console.log(a);
+
+const obj1={x:15,y:18};
+console.log(obj1);
+obj1.x=25;
+obj1.y=28;
+console.log(obj1);  // this is allowed
+
+
+// method to achive immutability
+//object.seal : updation is allowed but deletion and additon of data is not allowed
+
+//object.freeze: can not add,delete or update existing key value pair
+
