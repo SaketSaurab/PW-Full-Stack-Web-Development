155ea663ae92c6096689aee2952f0a6916ffeca3
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-07 16:23:09 +0530

    immediately invoke function

--- Practice/JavaScript/Anonymous Function.js
+++ Practice/JavaScript/Anonymous Function.js
@@ -1,1 +1,15 @@
-//Ananymous Function
+//Ananymous Function : Something that dont have name or identity
+let x=function(){
+    console.log("hi");
+}
+x();
+
+//immediately invoke function
+(function exec(){
+    console.log("Hello")
+})();
+
+//2nd example
+(function (x){
+    console.log(x*x);
+})(5);
