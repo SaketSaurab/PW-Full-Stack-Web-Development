50b8d2455133807f8287266d844de51073bb2b5b
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-07 19:08:07 +0530

    call back and high order functions

--- Practice/JavaScript/Hight order functions and callbacks.js
+++ Practice/JavaScript/Hight order functions and callbacks.js
@@ -1,1 +1,25 @@
 //high order functions and callbacks
+
+// function which accept another function is called high order function
+//function which is used inside the function is called callback
+function h(x,fn){
+    //h-> is higher order function
+    //fn-> is call back function
+    console.log(x*x);
+    fn();
+}
+h(10,function(){
+    console.log("done with call back");
+})
+
+console.log("timer function");
+
+console.log("Start");
+setTimeout(function f(){
+    console.log("timer done");
+},3000);
+console.log("end");
+
+
+
+
