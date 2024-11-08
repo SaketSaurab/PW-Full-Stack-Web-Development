15ba964209bc11b6a04dc45206d99c926ae41202
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-08 16:21:38 +0530

    lexical scope

--- Practice/JavaScript/Lexical Scopes.js
+++ Practice/JavaScript/Lexical Scopes.js
@@ -1,8 +1,15 @@
-// Lexical scope
-var currentStudent="Sanket";
-console.log(bestFriend);
-function updateStudentInfo(){
-    var currentStudentLocal="Sarthak";
-    bestFriend="tanmay";
-    console.log(`the best  friend of ${currentStudentLocal} is ${bestFriend} `);
+// // Lexical scope
+// var currentStudent="Sanket";
+// console.log(bestFriend);
+// function updateStudentInfo(){
+//     var currentStudentLocal="Sarthak";
+//     bestFriend="tanmay";
+//     console.log(`the best  friend of ${currentStudentLocal} is ${bestFriend} `);
+// }
+
+function fun(){
+    console.log("first", x);
+    var x=10; // if we use let then it will throw error 
+    console.log("second ",x);
 }
+fun();
