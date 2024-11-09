99ef74d869d069406d6b99a96e0dd3206e9389da
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-09 06:35:37 +0530

    oops1

---
+++ Practice/JavaScript/OOPS.js
@@ -0,0 +1,15 @@
+//oops 1
+//classes: are  blueprint
+/*
+    class NameOfTheClass {
+    // you can add details like  member functions and data members
+    }
+*/
+class Product{
+    addToCart(){
+        console.log("added to the cart");
+    }
+    removeFromCart(){
+        console.log("removed from the cart");
+    }
+}
