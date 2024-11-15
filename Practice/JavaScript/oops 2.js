5d5dfa312e34831842227e6556f956a0b43fc284
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-15 13:03:29 +0530

    oops2

--- Practice/JavaScript/oops 2.js
+++ Practice/JavaScript/oops 2.js
@@ -12,5 +12,11 @@ class product{
 we use getter setter method will help us to write validation logic
 
 class as wrappers on functions
+//*/
+
+//get description
+/* 
+accessing setter iphone.description 
+every class in js is wrapper over function
 
 */
