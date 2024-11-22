10b5acf7d2ac5a33168ffb038ac6d0d731b10246
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-22 09:14:12 +0530

    dom 2

--- Youtube Lecutres/JavaScript/DOM 2/DOM 2 Script.js
+++ Youtube Lecutres/JavaScript/DOM 2/DOM 2 Script.js
@@ -1,8 +1,11 @@
 //DOM 2
-let div=document.querySelector("div");
-console.log(div);
-let id =div.getAttribute("id");
-console.log(id);
+// let div=document.querySelector("div");
+// console.log(div);
+// let id =div.getAttribute("id");
+// console.log(id);
 
-let name=div.getAttribute("name");
-console.log(name);
+// let name=div.getAttribute("name");
+// console.log(name);
+
+let para=document.querySelector("p");
+console.log(para.setAttribute("para","newwClass"));
