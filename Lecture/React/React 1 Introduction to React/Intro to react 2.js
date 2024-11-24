49076983cb13e6c9cdcd9654e9d5884bc87bbf87
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-11-24 07:47:01 +0530

    React 1 L2

--- Lecture/React/React 1 Introduction to React/Intro to react 2.js
+++ Lecture/React/React 1 Introduction to React/Intro to react 2.js
@@ -1,9 +1,15 @@
-/* Component driven architchture
+/*react works on  Component driven architchture
 Cobination of mutliple react elements create components.
 React element is an object that describes the properties of an actual dom node which will be crated by react
 
+simple structure of react element......
+
 eg. {
     type: 'button'
+    props:{
+        className:"btn"
+    };
+
     ..
     ..
     }
