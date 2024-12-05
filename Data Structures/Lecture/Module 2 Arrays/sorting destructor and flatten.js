979618cd084157b51a006294f10f7e874aa01562
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-05 21:24:29 +0530

    sorting

---
+++ Data Structures/Lecture/Module 2 Arrays/sorting destructor and flatten.js
@@ -0,0 +1,8 @@
+//let sortedArray=numArray.sort();
+
+const Products=[
+    {name:"wireless mouse",price:522.5, inStock:true,},
+    {name:"Keyboard",price:799.5,inStock:true,},
+    {name:"data cable",price:129.9,inStock:true,},
+    {name:"aux cable",price:249,inStock:false,}
+    ];
