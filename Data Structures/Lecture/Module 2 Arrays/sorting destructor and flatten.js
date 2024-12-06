743516778ceacbe9e41f1e0315289de29b19677f
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-06 17:24:42 +0530

    Sorting,Destructor and Flatten

--- Data Structures/Lecture/Module 2 Arrays/sorting destructor and flatten.js
+++ Data Structures/Lecture/Module 2 Arrays/sorting destructor and flatten.js
@@ -6,3 +6,29 @@ const Products=[
     {name:"data cable",price:129.9,inStock:true,},
     {name:"aux cable",price:249,inStock:false,}
     ];
+//the sort() method sort the elements of an array 
+// let numArray=[35,63,73,23,67,34,9,56,43,234];
+// console.log("before sorting ",numArray);
+// let sortedArray=numArray.sort();
+// console.log("after sorting ",sortedArray);
+
+const sortedProduct=Products.slice().sort((a,b)=>a.price-b.price);
+console.log(sortedProduct);
+
+/// Destructuring
+console.log("Destructuring");
+const [{ name:FirstProductName,Price:FirstProductPrice}]=Products;
+console.log(FirstProductName);
+
+const product={name:"Keyboard",price:399.9,inStock:false}
+const {name,price,inStock}=product;
+console.log(name,price,inStock);
+
+
+//  Flatten Arrays:
+console.log("Flatten Arrays");
+const nestArray=[3,[35,[43,74],27,98],22];
+const flatArray=nestArray.flat(Infinity);
+console.log(flatArray);
+
+
