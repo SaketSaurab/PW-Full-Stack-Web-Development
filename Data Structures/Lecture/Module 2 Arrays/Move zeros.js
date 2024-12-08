ecc454ae26c27b809827e19f5265f399045d6a47
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-08 22:36:17 +0530

    move zeros method 2

--- Data Structures/Lecture/Module 2 Arrays/Move zeros.js
+++ Data Structures/Lecture/Module 2 Arrays/Move zeros.js
@@ -1,10 +1,26 @@
 // move all the zeros at the end
 
 // method 1
+// function moveZeros(nums){
+//     const nonZerosElements=nums.filter((num)=>num !=0);
+//     const zeroCount=nums.length-nonZerosElements.length;
+//     return [...nonZerosElements,...Array(zeroCount).fill(0)];
+// }
+// const nums=[3,5,0,5,0,0,2,3];
+// console.log(moveZeros(nums));
+
+//method 2
 function moveZeros(nums){
-    const nonZerosElements=nums.filter((num)=>num !=0);
-    const zeroCount=nums.length-nonZerosElements.length;
-    return [...nonZerosElements,...Array(zeroCount).fill(0)];
+    let lastNonZerosFound=0;
+    for(cur=0;cur<nums.length;cur++){
+        if(nums[cur]!=0){
+            [nums[lastNonZerosFound],nums[cur]]=[nums[cur],nums[lastNonZerosFound]];
+            lastNonZerosFound++;
+        }
+    }
+    return nums;
 }
+
 const nums=[3,5,0,5,0,0,2,3];
+console.log("original array",nums);
 console.log(moveZeros(nums));
