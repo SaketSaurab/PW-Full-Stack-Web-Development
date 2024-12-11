c4f93c960eb315e21de6202b51a2c20e763b298d
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-11 22:11:57 +0530

    variables

---
+++ Youtube Lecutres/JavaScript/Lecture 1 Variables and data types/Variables in javaScript.js
@@ -0,0 +1,9 @@
+// variables in javaScript
+fullName="Tony Stark";
+age=24;
+price=299.99;
+x=null;
+y=undefined;
+console.log(fullName,price);
+console.log("Null:",x);
+console.log("undefined:",y);
