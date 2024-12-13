595251e80ae0d304d474487173c4cc6f5bade7e3
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-13 08:29:51 +0530

    template literals

--- Youtube Lecutres/JavaScript/Lecture 3 Loops and strings/strings.js
+++ Youtube Lecutres/JavaScript/Lecture 3 Loops and strings/strings.js
@@ -1,10 +1,15 @@
 // Strings
-let str="hello strings";
-console.log(str);
 
-const name1=prompt("enter your name");
-console.log("Your name is ",name1);
+// let str="hello strings";
+// console.log(str);
 
-let strlen=name1.length;
-console.log("length of your name is",strlen);
-//
+// const name1=prompt("enter your name");
+// console.log("Your name is ",name1);
+
+// let strlen=name1.length;
+// console.log("length of your name is",strlen);
+
+//template literals
+console.log("template literals");
+let templateString1=`this is template literals it uses back tick symbol which is on the left top side of keyboard`;
+console.log(templateString1);
