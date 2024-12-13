1d93d53982dac67bc589d307ccf977c85e014b7c
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-13 08:24:22 +0530

    strings

---
+++ Youtube Lecutres/JavaScript/Lecture 3 Loops and strings/strings.js
@@ -0,0 +1,10 @@
+// Strings
+let str="hello strings";
+console.log(str);
+
+const name1=prompt("enter your name");
+console.log("Your name is ",name1);
+
+let strlen=name1.length;
+console.log("length of your name is",strlen);
+//
