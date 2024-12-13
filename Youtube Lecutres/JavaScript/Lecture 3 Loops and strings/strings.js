18c6c38dc24023eeb0be5b85d08a2828ac765aeb
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-13 09:03:59 +0530

    string interpolation

--- Youtube Lecutres/JavaScript/Lecture 3 Loops and strings/strings.js
+++ Youtube Lecutres/JavaScript/Lecture 3 Loops and strings/strings.js
@@ -1,15 +1,28 @@
 // Strings
 
-// let str="hello strings";
-// console.log(str);
+let str="hello strings";
+console.log(str);
 
-// const name1=prompt("enter your name");
-// console.log("Your name is ",name1);
+const name1=prompt("enter your name");
+console.log("Your name is ",name1);
 
-// let strlen=name1.length;
-// console.log("length of your name is",strlen);
+let strlen=name1.length;
+console.log("length of your name is",strlen);
 
 //template literals
 console.log("template literals");
-let templateString1=`this is template literals it uses back tick symbol which is on the left top side of keyboard`;
+let templateString1=`this is template literals it uses "back tick" symbol which is on the left top side of keyboard`;
 console.log(templateString1);
+
+let obj={
+    item:"pen",
+    price:10
+};
+
+console.log("normal method to print the item name and price");
+console.log("the item is",obj.item," and the price is",obj.price,"rupees");
+console.log("using backtick / template literal method");
+console.log(`the item is ${obj.item} and price is ${obj.price} rupees`);
+
+// the method of using ${expression} is called string interpolation
+
