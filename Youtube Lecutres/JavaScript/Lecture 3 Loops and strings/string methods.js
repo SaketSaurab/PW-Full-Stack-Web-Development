5bfd6c1312d18cafb0fce6086e823f1462eb9f1d
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-14 09:18:03 +0530

    string methods

---
+++ Youtube Lecutres/JavaScript/Lecture 3 Loops and strings/string methods.js
@@ -0,0 +1,10 @@
+// string methods
+let str="  Apna  College    ";
+console.log("original string:",str);
+
+// str.toUpperCase
+console.log("toUpperCase:",str.toUpperCase());
+
+console.log("toLowerCase:",str.toLocaleLowerCase());
+
+console.log("trim:",str.trim());
