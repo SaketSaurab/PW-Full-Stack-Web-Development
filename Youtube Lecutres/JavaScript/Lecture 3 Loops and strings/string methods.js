553407f68131874074f0c15253eaa02c14d35727
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-14 09:28:36 +0530

    charAt method

--- Youtube Lecutres/JavaScript/Lecture 3 Loops and strings/string methods.js
+++ Youtube Lecutres/JavaScript/Lecture 3 Loops and strings/string methods.js
@@ -1,5 +1,7 @@
 // string methods
-let str="  Apna  College    ";
+let str="  Apna College    ";
+
+let str2="pw skills";
 console.log("original string:",str);
 
 // str.toUpperCase
@@ -8,3 +10,10 @@ console.log("toUpperCase:",str.toUpperCase());
 console.log("toLowerCase:",str.toLocaleLowerCase());
 
 console.log("trim:",str.trim());
+
+
+console.log("str.slice(strt,end):",str.slice(3,9));
+console.log("str concat str2:",str.concat(str2));
+console.log("str.replace(searchValue,newValue):",str.replace('a','z'));
+console.log("str.charAt[index]",str.charAt(3));
+
