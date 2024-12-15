712c9d63cedf97dd6e18f6e28906bc1cbf449cce
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-15 10:43:22 +0530

    arrays

---
+++ Youtube Lecutres/JavaScript/Lecture 4 Arrays/arrays.js
@@ -0,0 +1,6 @@
+// arrays
+let heros=["iron man","hulk","captain america","deadpood"];
+console.log("super herors names:",heros);
+let marks=[34,63,73,86,45,32,13,67,98];
+console.log("marks",marks);
+console.log("length:",marks.length);
