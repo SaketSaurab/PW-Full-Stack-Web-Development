664d338279a35b5b817c9b975e2fa4e59021528c
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-15 11:30:54 +0530

    looping over an array

--- Youtube Lecutres/JavaScript/Lecture 4 Arrays/arrays.js
+++ Youtube Lecutres/JavaScript/Lecture 4 Arrays/arrays.js
@@ -4,3 +4,12 @@ console.log("super herors names:",heros);
 let marks=[34,63,73,86,45,32,13,67,98];
 console.log("marks",marks);
 console.log("length:",marks.length);
+
+// array indices
+console.log("marks[2]:",marks[2]);
+
+/// looping over an array
+console.log("looping over an array");
+for(let i=0;i<heros.length;i++){
+    console.log(heros[i]);
+}
