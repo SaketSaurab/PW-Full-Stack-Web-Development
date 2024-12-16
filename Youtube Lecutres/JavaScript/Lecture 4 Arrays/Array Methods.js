adfa18be1c2794e70249c4d7c30019c84aec37e4
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-16 12:05:29 +0530

    Array methods

---
+++ Youtube Lecutres/JavaScript/Lecture 4 Arrays/Array Methods.js
@@ -0,0 +1,14 @@
+// Array Methods
+let fruit=["apple","banana","lichi","mango","Pineapple","blueberry","blackberry"];
+console.log(`original list:${fruit}`);
+// Push(): add to the end
+fruit.push("strawberry","paneer");
+console.log(`list after pop ${fruit}`);
+// Pop(): delete from the end
+console.log("pop");
+fruit.pop();
+console.log(`list after pop ${fruit}`);
+
+fruit.toString();
+console.log("after toString",fruit);
+
