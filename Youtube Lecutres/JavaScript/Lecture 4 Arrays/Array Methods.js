929e474fc4be0a7574a9afc99580a3b9aea0bbaa
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-16 12:37:03 +0530

    slice and splice

--- Youtube Lecutres/JavaScript/Lecture 4 Arrays/Array Methods.js
+++ Youtube Lecutres/JavaScript/Lecture 4 Arrays/Array Methods.js
@@ -21,3 +21,16 @@ console.log("concat :",twoArray);
 // unshift method works as push method : add element in starting
 heros.unshift("spider man","HanuMan");
 console.log("after unshift:",heros);
+
+//shift method will delete the element from starting
+heros.shift();
+console.log(`after shift:${heros}`);
+
+// slice
+let slice1to3=heros.slice(1,3);
+console.log(`slice(1,3):${slice1to3}`);
+
+//splice used to add, remove ,replace
+console.log("splice");
+heros.splice(2,3,"345","567","678");
+console.log(heros);
