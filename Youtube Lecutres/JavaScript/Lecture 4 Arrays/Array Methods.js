5ef18d7e3a11276b186051bf86fcab01828c9a28
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-16 12:18:48 +0530

    unshift method

--- Youtube Lecutres/JavaScript/Lecture 4 Arrays/Array Methods.js
+++ Youtube Lecutres/JavaScript/Lecture 4 Arrays/Array Methods.js
@@ -1,5 +1,6 @@
 // Array Methods
 let fruit=["apple","banana","lichi","mango","Pineapple","blueberry","blackberry"];
+let heros=["iron man","bat man","superman","deadpool"];
 console.log(`original list:${fruit}`);
 // Push(): add to the end
 fruit.push("strawberry","paneer");
@@ -12,3 +13,11 @@ console.log(`list after pop ${fruit}`);
 fruit.toString();
 console.log("after toString",fruit);
 
+
+// Concat method to join multiple strings
+let twoArray=fruit.concat(heros);
+console.log("concat :",twoArray);
+
+// unshift method works as push method : add element in starting
+heros.unshift("spider man","HanuMan");
+console.log("after unshift:",heros);
