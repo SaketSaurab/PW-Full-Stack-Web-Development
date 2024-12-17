51bf059a4d87905ff0d6964f29421b90bb11fc95
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-17 08:08:25 +0530

    sum function

--- Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/Functions.js
+++ Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/Functions.js
@@ -1,1 +1,17 @@
 // functions
+
+// function definition
+function myFunction(){
+    console.log("Hello everyone");
+    console.log("You are learning JavaScript");
+}
+
+// function call
+myFunction();
+
+
+function sum(a,b){
+    console.log(`the sum of ${a} and ${b} is ${a+b}`);
+}
+
+sum(4,8);
