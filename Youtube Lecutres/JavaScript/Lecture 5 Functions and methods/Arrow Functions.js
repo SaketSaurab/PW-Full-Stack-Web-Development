54c1e6a9e7ee17910a14ad6130824f080907f732
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-18 08:31:15 +0530

    multiplication function using arrow function

--- Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/Arrow Functions.js
+++ Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/Arrow Functions.js
@@ -1,1 +1,22 @@
-// Arrow Function
+// Arrow Function: compact way of writing a function
+//  sum function
+// const functionName=(para1,para2,para3)=>{
+  //     do some calculation  
+//  }
+
+const arrowSum=(a,b)=>{
+    return a+b;
+};
+console.log("arrow sum:",arrowSum(4,5));
+
+// multiplication arrow function
+const arrowMultiply=(c,d,e)=>{
+    let ans=c*d*e;
+    console.log(`the multiplication of ${c},${d} and ${e} is ${ans}`);
+};
+
+arrowMultiply(3,4,6);
+
+// printing hello world using arrow function
+const hello=()=>console.log("hello world using arrow function and single line");
+hello();
