43032d493ff1bf73703ab879c322b3f4fdd2ce93
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-18 11:03:24 +0530

    arr.forEach()

--- Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/for each loop in arrays.js
+++ Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/for each loop in arrays.js
@@ -4,7 +4,22 @@ console.log("for each loop");
 arr.forEach((val)=>{
     console.log(var)});
 */
+
+
 let arr=[3,5,6,3,4,6,7,45,743,876,456,876,23,65];
+// forEach using call back function
+console.log("forEach using call back function");
 arr.forEach(function printFucntion(val){
     console.log(val);
+});
+
+// forEach using arrow function
+console.log("forEach using arrow function");
+arr.forEach((val) => {
+    console.log(val);
+});
+
+let arr2=["delhi","pune","mumbai","goa","manali","nainital"];
+arr2.forEach((val2,index) => {
+    console.log(val2.toUpperCase(),index);
 });
