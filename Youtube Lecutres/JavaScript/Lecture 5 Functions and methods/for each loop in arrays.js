220a4f006aad72b1abee82b2f6ade73c082c182b
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-18 10:46:38 +0530

    forEach loop

--- Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/for each loop in arrays.js
+++ Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/for each loop in arrays.js
@@ -1,1 +1,10 @@
 // for each loop in arrays
+console.log("for each loop");
+/*
+arr.forEach((val)=>{
+    console.log(var)});
+*/
+let arr=[3,5,6,3,4,6,7,45,743,876,456,876,23,65];
+arr.forEach(function printFucntion(val){
+    console.log(val);
+});
