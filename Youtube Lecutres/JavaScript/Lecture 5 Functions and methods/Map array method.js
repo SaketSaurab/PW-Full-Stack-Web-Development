43641256d50228f8caf3d4ff0fb274932157d7be
Saket Saurabh <100rabhsinghalld@gmail.com>
2024-12-19 12:26:21 +0530

    filter method

--- Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/Map array method.js
+++
@@ -1,7 +0,0 @@
-// map array method
-let arr1=[3,52,76,35,98,6,45];
-arr1.map((val1)=>{
-console.log(val1);
-});
-
-
---
+++ Youtube Lecutres/JavaScript/Lecture 5 Functions and methods/more array method.js
@@ -0,0 +1,16 @@
+// map array method
+let arr1=[3,52,76,35,98,6,45];
+arr1.map((val1)=>{
+console.log(val1);
+});
+
+
+    let arr2=[65,32,76,34,65,34,65,123,43,345,76,987,654];
+    let newArr1=arr2.map((val2)=>{
+        return val2;
+    });
+console.log("using new array:",newArr1);
+
+
+// filter method 
+console.log("filter method");
