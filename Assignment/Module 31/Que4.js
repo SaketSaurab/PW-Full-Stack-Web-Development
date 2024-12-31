7ecbfcc1d330b3b1016d14ce256e60c59cad4e86
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-01 00:23:38 +0530

    Module 31 Question 4

---
+++ Assignment/Module 31/Que4.js
@@ -0,0 +1,9 @@
+let car = {
+    brand: 'Tata',
+    model: 'Nexon',
+    displayDetails: function() {
+        console.log(`Brand: ${this.brand}, Model: ${this.model}`);
+    }
+};
+
+car.displayDetails(); 
