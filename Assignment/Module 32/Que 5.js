c364a7e5c8663680491ff96b098168e96fc030bf
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-04 07:51:00 +0530

    Module 32 Question 5 Solution

--- Assignment/Module 32/Que 5.js
+++ Assignment/Module 32/Que 5.js
@@ -2,3 +2,15 @@
  * Create a simple polyfill for the Array.includes method by the name of customIncludes.
 
  */
+
+if (!Array.prototype.customIncludes) {
+    Array.prototype.customIncludes = function(element) {
+        return this.some(function(el) {
+            return el === element;
+        });
+    };
+}
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.customIncludes(3)); 
+console.log(arr.customIncludes(6)); 
