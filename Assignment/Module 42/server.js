6e1f69be2d5d78e8e793946256e8d27369e933ab
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-05 08:14:05 +0530

    Module 42 Q1 Solution

--- Assignment/Module 42/server.js
+++ Assignment/Module 42/server.js
@@ -2,3 +2,17 @@
  * Write a Node.js program that creates a simple HTTP server. When a client sends a request to the server,
 the server should respond with a "Hello, Students!" message#
  */
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, Students!');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
