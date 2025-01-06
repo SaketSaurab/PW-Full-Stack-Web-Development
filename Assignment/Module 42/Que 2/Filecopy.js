630f9c845986d6bb8872d8e2e07298a75a84417b
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-06 08:18:58 +0530

    Module 42 Que 2  Solution

--- Assignment/Module 42/Que 2/Filecopy.js
+++ Assignment/Module 42/Que 2/Filecopy.js
@@ -1,3 +1,18 @@
 /**
  * Write a Node.js program that reads content from a file named " " and writes the content to a new file named " "#
  */
+const fs = require('fs');
+
+fs.readFile('source.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading source file:', err);
+        return;
+    }
+    fs.writeFile('destination.txt', data, 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing to destination file:', err);
+            return;
+        }
+        console.log('Content copied successfully!');
+    });
+});
