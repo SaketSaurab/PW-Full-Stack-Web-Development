ca45e1f74e3accd7767e6a1469221e772be5a56f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-07 08:22:22 +0530

    Module 42 Que 3

---
+++ Assignment/Module 42/Que 3/RouteServer.js
@@ -0,0 +1,4 @@
+/**
+ * Write a Node.js program that creates an HTTP server and handles different routes. The server should
+respond with "Hello, World!" for the root route ("/") and "Page Not Found" for any other route#
+ */
