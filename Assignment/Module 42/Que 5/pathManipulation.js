b7f4ebde4aeb98c3c95d4a6c8d1157c6032d64ff
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-09 08:30:51 +0530

    Module 42 Que 5 Solution

--- Assignment/Module 42/Que 5/pathManipulation.js
+++ Assignment/Module 42/Que 5/pathManipulation.js
@@ -1,3 +1,19 @@
 /**
  * Write a Node.js program that uses the Path module to manipulate file paths#
  */
+const path = require('path');
+
+const joinedPath = path.join('/users', 'sandeep', 'documents', 'file.txt');
+console.log(`Joined Path: ${joinedPath}`);
+
+const resolvedPath = path.resolve('users', 'sandeep', 'documents', 'file.txt');
+console.log(`Resolved Path: ${resolvedPath}`);
+
+const dirname = path.dirname('/users/sandeep/documents/file.txt');
+console.log(`Directory Name: ${dirname}`);
+
+const basename = path.basename('/users/sandeep/documents/file.txt');
+console.log(`Base Name: ${basename}`);
+
+const extname = path.extname('/users/sandeep/documents/file.txt');
+console.log(`Extension Name: ${extname}`);
