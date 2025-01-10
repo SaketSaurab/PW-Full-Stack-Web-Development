574181c65af9a8f9009705deec29bfc0d765a910
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-10 08:32:34 +0530

    Que 6

---
+++ Assignment/Module 42/Que 6/caculator.js
@@ -0,0 +1,4 @@
+/**
+ * # Create a command-line Node.js program that simulates a basic calculator. The program should allow
+users to perform addition, subtraction, multiplication, and division operations on two numbers.
+ */
