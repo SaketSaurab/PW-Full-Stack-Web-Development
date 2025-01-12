ab67ea5d2fa4560ea8a49da597ffb80a20fccad1
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-12 17:37:07 +0530

    string and number conversion

---
+++ Java Script Codes/1 Basics of JavaScript/1.1 Variables and Data types/3 Convert a number to a string and vice versa.js
@@ -0,0 +1,7 @@
+//Convert a number to a string and vice versa
+
+let a=20;
+console.log(`value of a=${a} and type of a=${typeof(a)}`);
+
+let b=String(a);
+console.log(`value of a=${b} and type of a=${typeof(b)}`);
