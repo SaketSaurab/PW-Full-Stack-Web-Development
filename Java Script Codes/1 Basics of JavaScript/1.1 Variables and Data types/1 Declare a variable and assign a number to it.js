6b042afe54cf161aa761e4497fd18278a4f134b4
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-12 17:28:38 +0530

    variable declaration

---
+++ Java Script Codes/1 Basics of JavaScript/1.1 Variables and Data types/1 Declare a variable and assign a number to it.js
@@ -0,0 +1,7 @@
+// Declare a variable and assign a number to it.
+
+let a=2; // declaring a variable using let, we can later reassign another value in this 
+const b=5;  // declaring a variable using const , but we can not change the value later.
+var c=15; // declaring a variable using var, it was old method used to declare the variable but now this is not used 
+
+console.log(`variable declared using let a=${a} \n variable declare using const b=${b} \n variable declared using var c=${c}`);
