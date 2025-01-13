0e6805c2b1d631a0394b7a9f5f49dff9be8a6a15
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-13 17:43:19 +0530

    variable is undefined or null

---
+++ Java Script Codes/1 Basics of JavaScript/1.1 Variables and Data types/4 Check if a variable is undefined or null.js
@@ -0,0 +1,6 @@
+// Check if a variable is undefined or null
+
+let a;  // the value is assigned so it is undefined 
+let x=null;  
+
+console.log(`the value of a=${a} and type of a=${typeof(a)}\n vlaue of x=${x} and type of x = ${typeof(x)}`);
