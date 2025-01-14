f3c6ae6d6ca73b5aba5d3b49de5692a7959b60d6
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-14 18:19:46 +0530

    string to number conversion

---
+++ Java Script Codes/1 Basics of JavaScript/1.1 Variables and Data types/6 Convert a string to a number.js
@@ -0,0 +1,7 @@
+// Convert a string to a number
+
+let a="123";
+console.log(`the value of a${a} and the type of a is ${typeof(a)}`);
+
+let b=String(a);
+console.log(`the value of b=${b} and the type of b is ${typeof(b)}`);
