312d5ca16883c528854633e16c8a6ebeb9286895
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-16 20:16:07 +0530

    arithmatic operation between two numbers

---
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/1 Perform all arithmetic operations on two numbers.js
@@ -0,0 +1,37 @@
+// Perform all arithmetic operations on two numbers
+
+function sum(a,b){
+    console.log(`the sum of ${a} and ${b} is ${a+b}`);
+}
+
+function subtraction(a,b){
+    console.log(`the subtraction of ${a} and ${b} is ${a-b}`);
+}
+
+function multiplication(a,b){
+    console.log(`the multiplication of ${a} and ${b} is ${a*b}`);
+}
+
+function division(a,b){
+    console.log(`the division of ${a} and ${b} is ${a/b}`);
+}
+
+function modulo(a,b){
+    console.log(`the modulo of ${a} and ${b} is ${a%b}`);
+}
+
+function exponetial(a,b){
+    console.log(`the exponential of ${a} and ${b} is ${a**b}`);
+}
+
+let x=50
+let y=13;
+let a=12;
+let b=4;
+
+sum(x,y);
+subtraction(x,y);
+multiplication(x,y);
+division(x,y);
+modulo(x,y);
+exponetial(a,b);
