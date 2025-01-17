fea52e393c61b0d96f0a3ca085ee2a722c2f2fe5
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-17 20:19:59 +0530

    is numbers are equal or not

---
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/2 Write a program to check if two numbers are equal.js
@@ -0,0 +1,19 @@
+// Write a program to check if two numbers are equal
+
+function isNumEqula(a,b){
+    if(a==b){
+        console.log(`the numbers ${a} and ${b} are equal`);
+    }
+    else {
+        console.log(`the numbers ${a} and ${b} are not equla`);
+    }
+}
+
+let x1=15;
+let y1=22;
+
+let x2=22;
+let y2=22;
+
+isNumEqula(x1,y1);
+isNumEqula(x2,y2);
