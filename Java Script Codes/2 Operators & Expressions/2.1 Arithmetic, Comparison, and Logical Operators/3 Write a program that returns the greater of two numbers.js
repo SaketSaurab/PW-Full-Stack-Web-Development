ada35592b4af1f7c231089e6c897f3655a3ab20b
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-17 22:13:22 +0530

    biggest number in two numbers

--- Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/3 Write a program that returns the greater of two numbers.js
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/3 Write a program that returns the greater of two numbers.js
@@ -1,1 +1,8 @@
 // Write a program that returns the greater of two numbers
+function greaterNumber(a,b){
+    console.log(`${a>b?a:b} is the biggest number in ${a} and ${b}`)
+}
+
+let x=23;
+let y=68;
+greaterNumber(x,y);
