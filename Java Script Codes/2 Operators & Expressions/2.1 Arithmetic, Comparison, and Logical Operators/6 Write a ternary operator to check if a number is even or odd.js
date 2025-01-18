e44247749fa6236b83397964e4fb1c01913ff90a
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-18 23:31:52 +0530

    check even or odd using ternary operator

---
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/6 Write a ternary operator to check if a number is even or odd.js
@@ -0,0 +1,11 @@
+// Write a ternary operator to check if a number is even or odd.
+
+function evenOrOdd(a){
+    console.log(`${a} is ${a%2==0?"even":"odd"}`);
+}
+
+let x=23;
+let y=92;
+
+evenOrOdd(x);
+evenOrOdd(y);
