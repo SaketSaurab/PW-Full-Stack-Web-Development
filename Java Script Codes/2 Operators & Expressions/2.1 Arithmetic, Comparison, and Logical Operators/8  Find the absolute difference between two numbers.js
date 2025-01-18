d3d8dea52fac739c1d546d2a2d7ff46c6e0bf00f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-18 23:48:15 +0530

    absolute differnce program

---
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/8  Find the absolute difference between two numbers.js
@@ -0,0 +1,12 @@
+//  Find the absolute difference between two numbers
+
+function absoluteDifference(a,b){
+    console.log(`the absolute difference between ${a} and ${b} is ${Math.abs(a-b)}`)
+}
+
+let a=10;
+let b=20;
+let c=-10;
+let d=34;
+absoluteDifference(a,b);
+absoluteDifference(c,d);
