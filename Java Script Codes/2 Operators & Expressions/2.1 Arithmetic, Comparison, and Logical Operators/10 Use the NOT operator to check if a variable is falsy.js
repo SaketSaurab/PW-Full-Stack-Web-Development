ddbc39d35108b41643d0d89a5fdd8fecb9a54c80
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-20 00:05:35 +0530

    truthy or falsy check

--- Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/10 Use the NOT operator to check if a variable is falsy.js
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/10 Use the NOT operator to check if a variable is falsy.js
@@ -1,1 +1,22 @@
 // Use the NOT operator to check if a variable is falsy
+
+function isFalsy(a){
+    if(!a){
+        console.log(`${a} is falsy.`);
+    }
+    else{
+        console.log(`${a} is truthy`);
+    }
+}
+
+isFalsy(false);
+isFalsy(true);
+isFalsy(0);
+isFalsy(1);
+isFalsy(20);
+isFalsy("");
+isFalsy(" ");
+isFalsy("a");
+isFalsy(null);
+isFalsy(undefined);
+isFalsy(NaN);
