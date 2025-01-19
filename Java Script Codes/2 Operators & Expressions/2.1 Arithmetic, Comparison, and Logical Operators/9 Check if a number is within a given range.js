362b16b1d50dcff253710499735f0f137648c723
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-19 23:54:40 +0530

    range check

---
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/9 Check if a number is within a given range.js
@@ -0,0 +1,22 @@
+// Check if a number is within a given range
+
+function check(num1,num2,numCheck){
+    let max=num1;
+    let min=num2;
+    if(num2>num1){
+        max=num2;
+        min=num1;
+    }
+    if(numCheck<=max && numCheck<=min){
+        console.log(`the ${numCheck} is within the given range`);
+    
+    }
+    else{
+        console.log(`the ${numCheck} is not in the given range`);
+    }
+}
+
+let a=15;
+let b=5;
+let c=8;
+check(a,b,c);
