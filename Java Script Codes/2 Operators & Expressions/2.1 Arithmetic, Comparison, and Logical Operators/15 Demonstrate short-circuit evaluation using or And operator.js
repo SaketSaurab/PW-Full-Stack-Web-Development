1b16595ad46f59965cc397f26a6f67868f393f09
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-22 00:54:43 +0530

    short circuit evaluation

---
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/15 Demonstrate short-circuit evaluation using or And operator.js
@@ -0,0 +1,6 @@
+// Demonstrate short-circuit evaluation using && and ||.
+
+console.log("this is or short circuit ",true||"hello or");
+console.log("this is or short circuit ",false||"hello or");
+console.log("this is or short circuit ",false && "hello and");
+console.log("this is or short circuit ",true&&"hello and");
