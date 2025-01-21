87993572bb9bb6eeed28d5cbcb8b5014122b20fb
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-22 00:39:23 +0530

    Nullish coalescing operator

--- Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/14 Use the nullish coalescing operator to assign a default value..js
+++ Java Script Codes/2 Operators & Expressions/2.1 Arithmetic, Comparison, and Logical Operators/14 Use the nullish coalescing operator to assign a default value..js
@@ -1,1 +1,9 @@
 // Use the nullish coalescing operator to assign a default value.
+
+function NullishValue(x){
+    console.log(`${x!==null && x!==undefined?x:"default value"}`);
+}
+NullishValue("a");
+NullishValue(null);
+NullishValue(undefined);
+NullishValue(23);
