4ecf04e4a483f9defea46ba0bc34861e31e1291d
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-23 11:04:10 +0530

    largest of three numbers

---
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.1 If-Else and Switch Statement/1 Find the largest of three numbers using if-else.js
@@ -0,0 +1,19 @@
+// Find the largest of three numbers using if-else.
+
+function largest(a,b,c){
+    if(a>b){
+        if(a>=c && a>=b){
+            console.log(`The largest number among ${a},${b} and ${c} is ${a}`);
+        }
+    }
+    else if(b>=c && b>=a){
+        
+        console.log(`The largest number among ${a},${b} and ${c} is ${b}`);
+    }
+    else    {
+        
+        console.log(`The largest number among ${a},${b} and ${c} is ${c}`);
+    }
+}
+
+largest(13,16,15);
