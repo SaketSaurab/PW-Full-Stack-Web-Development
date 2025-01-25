6dbcb97a9bdad8dba3cd2f8b44b6ad9548f3a546
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-25 19:15:57 +0530

    Prime number check by taking input from user

--- Java Script Codes/3 Control Flow (Loops & Conditionals)/3.1 If-Else and Switch Statement/7 Write a program to check if a number is prime.js
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.1 If-Else and Switch Statement/7 Write a program to check if a number is prime.js
@@ -1,2 +1,12 @@
 // Write a program to check if a number is prime.
 
+const readline=require('readline');
+
+function PrimeOrNot(a){
+    
+}
+
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
