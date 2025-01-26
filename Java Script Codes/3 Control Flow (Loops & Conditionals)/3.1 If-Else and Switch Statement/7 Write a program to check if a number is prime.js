5c711c8ba2d09e4862c360fa27f726cad56b249f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-26 07:16:05 +0530

    Prime number check

--- Java Script Codes/3 Control Flow (Loops & Conditionals)/3.1 If-Else and Switch Statement/7 Write a program to check if a number is prime.js
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.1 If-Else and Switch Statement/7 Write a program to check if a number is prime.js
@@ -2,11 +2,39 @@
 
 const readline=require('readline');
 
-function PrimeOrNot(a){
-    
+function isPrime(a){
+    let prime=true;
+    if(isNaN(a) || a<2 || !Number.isInteger(a)){
+        console.log(`${a} is not a valid input for prime number check`);
+        return;
+    }
+        for(let i=2;i<=Math.sqrt(a);i++){
+            if(a%i===0){
+                prime=false;
+                break;
+            }
+        }
+
+    if(prime){
+        console.log(`${a} is a prime number`);
+    }
+    else{
+        console.log(`${a} is not a prime number`);
+    }
 }
 
 const rl=readline.createInterface({
     input:process.stdin,
     output:process.stdout
+});
+
+rl.question("Enter the number to check :",(num)=>{
+    const x=Number(num.trim());
+    if(!isNaN(x)){
+        isPrime(x);
+    }
+    else{
+        console.log(`${num} is not a valid input`);
+    }
+    rl.close();
 });
