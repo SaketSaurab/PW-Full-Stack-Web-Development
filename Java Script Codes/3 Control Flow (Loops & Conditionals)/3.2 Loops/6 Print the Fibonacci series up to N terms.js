1e27c070264d2a2e929a9ad4c28186a1550547ac
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-01-31 18:45:32 +0530

    fibonacci number

---
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/6 Print the Fibonacci series up to N terms.js
@@ -0,0 +1,17 @@
+// Print the Fibonacci series up to N terms
+
+const readline=require('readline');
+
+function fibo(x){
+    if(x=0){
+        console.log(x);
+    }
+    else if(x=1){
+        console.log("0, 1");
+    }
+    else if(x>1){
+        let a=0;
+        let b=1;
+        let c=a+b;
+    }
+}
