c74e8be0f5c6e079874523b94fedcac180002a2e
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-03 09:04:40 +0530

    some edge case fixed

--- Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/11 Count the number of digits in a number.js
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/11 Count the number of digits in a number.js
@@ -1,27 +1,32 @@
 // Count the number of digits in a number.
 
-const readline=require('readline');
-function countDigits(num){
-    let temp=num;
-    let totalDigit=0;
-    while(num>0){
-        totalDigit+=1;
-        num=Math.floor(num/10);
-    }
-    console.log(`Total number of digits in ${temp} is ${totalDigit}`);
+const readline = require("readline");
+function countDigits(num) {
+  let originalNumber = num;
+
+  if (num === 0) {
+    console.log(`Total number of digits in ${originalNumber} is 1`);
+    return;
+  }
+  num = Math.abs(num);
+  let totalDigit = 0;
+  while (num > 0) {
+    totalDigit += 1;
+    num = Math.floor(num / 10);
+  }
+  console.log(`Total number of digits in ${originalNumber} is ${totalDigit}`);
 }
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question("Enter the number to cont the digits : ",(input)=>{
-    if(/^-?\d+$/.test(input.trim())){
-        let num=parseInt(input.trim());
-        countDigits(num);
-    }
-    else{
-        console.log("Enter valid number ");
-    }
-    rl.close();
+rl.question("Enter the number to cont the digits : ", (input) => {
+  if (/^-?\d+$/.test(input.trim())) {
+    let num = parseInt(input.trim());
+    countDigits(num);
+  } else {
+    console.log("Enter valid number ");
+  }
+  rl.close();
 });
