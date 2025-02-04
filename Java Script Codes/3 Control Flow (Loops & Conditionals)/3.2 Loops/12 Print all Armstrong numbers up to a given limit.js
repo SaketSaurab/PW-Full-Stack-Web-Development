838e3b91421ef43a48321fac73a9c54276e73ab5
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-04 09:33:05 +0530

    Printing all armstrong number up to given number

---
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/12 Print all Armstrong numbers up to a given limit.js
@@ -0,0 +1,39 @@
+// Print all Armstrong numbers up to a given limit.
+
+const readline = require("readline");
+function PrintArmstrongNumber(a) {
+  for (let x = 1; x <= a; x++) {
+    let num = x;
+    let originalNumber = x;
+    let sizeOfNumber = 0;
+    let arm = 0;
+
+    while (num > 0) {
+      sizeOfNumber += 1;
+      num = Math.floor(num/ 10);
+    }
+    num=originalNumber;
+    while (num > 0) {
+      let temp = num % 10;
+      arm = arm + temp ** sizeOfNumber;
+      num = Math.floor(num / 10);
+    }
+    if(originalNumber===arm){
+        console.log(originalNumber);
+  }
+  }
+}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter the number to print Armstrong number ", (input) => {
+  if (/^\d+$/.test(input.trim())) {
+    let num = parseInt(input.trim());
+    PrintArmstrongNumber(num);
+  } else {
+    console.log("Enter valid number");
+  }
+  rl.close();
+});
