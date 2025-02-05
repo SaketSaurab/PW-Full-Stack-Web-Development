55d2d102365bd17b2d99e7b4e99932db1937a766
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-05 14:49:16 +0530

    GCD of two numbers

---
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/13 Find the Greatest Common Divisor (GCD) of two numbers.js
@@ -0,0 +1,34 @@
+//Find the Greatest Common Divisor (GCD) of two numbers
+const readline = require("readline");
+function gcd(a, b) {
+  let small;
+  if (a < b) {
+    small = a;
+  } else {
+    small = b;
+  }
+  let ans;
+  for (let i = 2; i <= small; i++) {
+    if (a % i == 0 && b % i == 0) {
+      ans = i;
+    }
+  }
+  console.log(`The GCD of ${a} and ${b} is ${ans}`);
+}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter first number : ", (input1) => {
+  rl.question("Enter second number : ", (input2) => {
+    if (/^\d+$/.test(input1.trim()) && /^\d+$/.test(input2.trim())) {
+      let num1 = parseInt(input1.trim());
+      let num2 = parseInt(input2.trim());
+      gcd(num1, num2);
+    } else {
+      console.log("Please enter valid number ");
+    }
+    rl.close();
+  });
+});
