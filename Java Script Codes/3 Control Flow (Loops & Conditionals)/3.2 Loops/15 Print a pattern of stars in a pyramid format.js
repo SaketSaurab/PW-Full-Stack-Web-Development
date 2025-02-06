81b2681bcef514803ec052836354c8cbd5640cbf
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-06 18:21:49 +0530

    printing pyramind using for loop

--- Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/15 Print a pattern of stars in a pyramid format.js
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/15 Print a pattern of stars in a pyramid format.js
@@ -1,1 +1,32 @@
 // Print a pattern of stars in a pyramid format.
+const readline = require("readline");
+
+function pyramid(a) {
+  for (let i = 1; i <= a; i++) {
+    let row = "";
+    for (let j = 1; j <= a - i; j++) {
+      row += "  ";
+    }
+    for (let j = 1; j <= i; j++) {
+      row += "* ";
+    }
+    for (let j = 2; j <=  i; j++) {
+      row += "* ";
+    }
+    console.log(row);
+  }
+}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter the height of the pyramind : ", (input) => {
+  if (/^\d+$/.test(input.trim())) {
+    let a = parseInt(input.trim());
+    pyramid(a);
+  } else {
+    console.log("Enter valid number");
+  }
+  rl.close();
+});
