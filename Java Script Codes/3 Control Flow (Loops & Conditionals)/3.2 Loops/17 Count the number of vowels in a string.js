e5d5d49f7db711e06d24def97f0220c6ad6634b9
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-07 18:56:21 +0530

    vowel count

--- Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/17 Count the number of vowels in a string.js
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/17 Count the number of vowels in a string.js
@@ -1,2 +1,15 @@
 // Count the number of vowels in a string.
 
+const readline=require('readline');
+function count(a){
+
+}
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,      
+    });
+    rl.question("Enter the string ",(input)=>{
+        let a=input.trim();
+        count(a);
+        rl.close();
+    })
