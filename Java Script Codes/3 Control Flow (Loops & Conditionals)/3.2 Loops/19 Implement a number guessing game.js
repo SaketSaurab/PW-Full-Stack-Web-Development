d0d4812d519eb77c2bbbc9f30cdf84a627e459dc
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-09 13:29:11 +0530

    Number guessing game using random number generator

--- Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/19 Implement a number guessing game.js
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/19 Implement a number guessing game.js
@@ -1,14 +1,34 @@
 // Implement a number guessing game.
 
-const readline=require('readline');
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
-function guessNumber(input){
 
+function guessNumber(randomNumber){
+rl.question("Guess the number : ",(input)=>{
+  if(!/^\d+$/.test(input.trim())){
+    console.log("Enter valid positive number");
+    guessNumber(randomNumber);
+  }
+  else{
+    let userInput=parseInt(input.trim());
+    if(userInput===randomNumber){
+      console.log(`you guess it right random number was : ${randomNumber}`);
+    }
+    else if(userInput<randomNumber){
+      console.log("enter bigger number");
+      guessNumber(randomNumber);
+    }
+    else if(userInput>randomNumber){
+      console.log("enter smaller number");
+      guessNumber(randomNumber);
+    }
+  }
+})
 }
 
-rl.question("Enter the number : ",(input)=>{
-
-})
+const randomNumber=Math.floor(Math.random()*100);
+console.log("Guess the number between 0 to 100");
+guessNumber(randomNumber);
