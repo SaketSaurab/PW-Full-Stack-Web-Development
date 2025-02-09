139cce306ccff157ed782eeb30cb58ba34730768
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-09 07:21:30 +0530

    random number generator

---
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/19 Implement a number guessing game.js
@@ -0,0 +1,14 @@
+// Implement a number guessing game.
+
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+function guessNumber(input){
+
+}
+
+rl.question("Enter the number : ",(input)=>{
+
+})
---
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/Random number generator.js
@@ -0,0 +1,14 @@
+// random number generator
+
+// generating random number 
+let randomNumber=Math.random();
+console.log("Random number : ",(randomNumber));
+
+// generating random number between 1 to 100
+// 1 = min
+// 100 = max
+let randomNumberBetween=Math.floor(Math.random()*(100-1+1))+1;
+console.log("Random number between 1 to 100 : ",(randomNumberBetween));
+
+let randomNumberBetween2=Math.floor(Math.random()*(500-50+1))+50;
+console.log("Random number between 50 to 500 : ",(randomNumberBetween2));
