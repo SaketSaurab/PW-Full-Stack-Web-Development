2d685ba918bf690b3fed02dd8db89608210022eb
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-11 17:41:25 +0530

    prime number check using function

--- Java Script Codes/4 Functions/4.1 Functions Basics/2  Write a function to check if a number is prime.js
+++ Java Script Codes/4 Functions/4.1 Functions Basics/2  Write a function to check if a number is prime.js
@@ -1,1 +1,38 @@
 //  Write a function to check if a number is prime
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function primeCheck(num) {
+  let check = true;
+  let a = num;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i == 0) {
+      check = false;
+      break;
+    }
+  }
+  if (check === true) {
+    console.log(`${a} is prime number`);
+  } else {
+    console.log(`${a} is not prime number `);
+  }
+}
+
+rl.question("Enter the number to check for prime : ", (input) => {
+  if (/^\d+$/.test(input.trim())) {
+    let num = parseInt(input.trim());
+    if (num == 0) {
+      console.log("Enter the number greater than  0 ");
+    } else if (num == 1 || num == 2) {
+      console.log(`The prime number of ${num} is ${num}`);
+    } else {
+      primeCheck(num);
+    }
+    rl.close();
+  } else {
+    console.log(`${input} is not valid number Please enter valid number `);
+  }
+});
