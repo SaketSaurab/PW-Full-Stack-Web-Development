0ca2ba728fa292b24b0ca33f6b4bceb2dcd5e013
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-11 17:57:54 +0530

    reversing the string using function

---
+++ Java Script Codes/4 Functions/4.1 Functions Basics/3 Write a function that returns the reverse of a string.js
@@ -0,0 +1,18 @@
+// Write a function that returns the reverse of a string.
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+function reverseString(input){
+    let str=input.trim();
+    let revStr="";
+    for(let i=str.length-1;i>=0;i--){
+        revStr+=String(str.charAt(i));
+    }
+    console.log(`The reverse of ${input} string is ${revStr}`);
+
+}
+rl.question("Enter the string : ",(input)=>{
+    reverseString(input);
+});
