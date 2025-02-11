9ff35070c4231774a92407de98219f3ef292536c
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-11 05:57:00 +0530

    string reverse

--- Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/20 Print the reverse of a string.js
+++ Java Script Codes/3 Control Flow (Loops & Conditionals)/3.2 Loops/20 Print the reverse of a string.js
@@ -4,3 +4,14 @@ const rl=readline.createInterface({
 input:process.stdin,
 output:process.stdout,
 });
+function reverseString(str){
+    let newString="";
+for(let i=str.length-1;i>=0;i--){
+    newString+=str.charAt(i);
+}
+console.log(newString);
+}
+rl.question("Enter the string : ",(input)=>{
+    reverseString(input);
+    rl.close();
+});
