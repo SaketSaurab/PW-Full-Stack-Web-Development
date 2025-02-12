00355a55d05a5826789e7baa3bbc1cdd364b3340
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-12 23:53:20 +0530

    function to conver fahrenheit ot celsius

--- Java Script Codes/4 Functions/4.1 Functions Basics/5 Create a function to convert Fahrenheit to Celsius.js
+++ Java Script Codes/4 Functions/4.1 Functions Basics/5 Create a function to convert Fahrenheit to Celsius.js
@@ -1,22 +1,21 @@
 // Create a function to convert Fahrenheit to Celsius.
-const readline=require('readline');
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-function fahrenheitToCelsius(f){
-    let c=(f-32)/(9/5);
-    c=c.toFixed(2);
-    console.log(`${f}\u00b0F = ${c}\u00b0C`);
+function fahrenheitToCelsius(f) {
+  let c = (f - 32) / (9 / 5);
+  c = c.toFixed(2);
+  console.log(`${f}\u00b0F = ${c}\u00b0C`);
 }
-rl.question("Enter the temprature in Fahrenheit : ",(input)=>{
-if(/^-?\d+(\.\d+)?$/.test(input.trim())){
-    let f=parseFloat(input.trim());
+rl.question("Enter the temprature in Fahrenheit : ", (input) => {
+  if (/^-?\d+(\.\d+)?$/.test(input.trim())) {
+    let f = parseFloat(input.trim());
     fahrenheitToCelsius(f);
-}
-else{
+  } else {
     console.log("Enter valid temprature");
-}
-rl.close();
+  }
+  rl.close();
 });
