3751ad15ead14224fd33820e7c1d33fe6cacdd8d
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-14 09:40:21 +0530

    sum of digits of number using function

--- Java Script Codes/4 Functions/4.1 Functions Basics/9 Write a function that returns the sum of digits of a number.js
+++ Java Script Codes/4 Functions/4.1 Functions Basics/9 Write a function that returns the sum of digits of a number.js
@@ -1,2 +1,25 @@
 // Write a function that returns the sum of digits of a number
-const readline=require('readline');
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+function sumOfDigits(input) {
+  let sum = 0;
+  let num = input;
+  while (input > 0) {
+    sum += input % 10;
+    input = Math.floor(input / 10);
+  }
+  console.log(`The sum of digits of ${num} is ${sum}`);
+}
+
+rl.question("Enter the number : ", (input) => {
+  if (/^-?\d+$/.test(input.trim())) {
+    let num = parseInt(input.trim());
+    sumOfDigits(num);
+  } else {
+    console.log("Enter valid number ");
+  }
+  rl.close();
+});
