d3da02754ce9113b7574493c5733f5794a41c11f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-16 08:36:21 +0530

    program to convert string to camel case using function

--- Java Script Codes/4 Functions/4.1 Functions Basics/14 Write a function that converts a given string to camel case.js
+++ Java Script Codes/4 Functions/4.1 Functions Basics/14 Write a function that converts a given string to camel case.js
@@ -1,26 +1,22 @@
 // Write a function that converts a given string to camel case.
-const readline=require('readline');
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,  
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
-function camelCase(input){
-    let str=input.toLowerCase();
-let words=str.trim().split(/\s+/);
-// console.log(`Words : ${words}`);
-let result="";
-for(let i=1;i<words.length;i++){
-    let word=words[i];
-    if(i==0){
-        result+=word;
-    }
-    result+=word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
+function camelCase(input) {
+  let str = input.toLowerCase();
+  let words = str.trim().split(/\s+/);
+  // console.log(`Words : ${words}`);
+  let result = words[0];
+  for (let i = 1; i < words.length; i++) {
+    let word = words[i];
+    result += word.charAt(0).toUpperCase() + word.slice(1);
+  }
+  console.log(result);
 }
-console.log(result);
-
-}
-rl.question("Enter the string to convert to camelCase ",(input)=>{
-    let str=input.trim();
-    camelCase(str);
-    rl.close();
+rl.question("Enter the string to convert to camelCase ", (input) => {
+  let str = input.trim();
+  camelCase(str);
+  rl.close();
 });
