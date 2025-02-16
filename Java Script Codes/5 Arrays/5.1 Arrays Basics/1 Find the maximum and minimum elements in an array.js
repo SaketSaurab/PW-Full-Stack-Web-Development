1f8c18b0be467481010200819c2e128098504a73
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-16 11:08:15 +0530

    Biggest number in array using function

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/1 Find the maximum and minimum elements in an array.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/1 Find the maximum and minimum elements in an array.js
@@ -1,2 +1,23 @@
 // Find the maximum and minimum elements in an array
 
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+function max(input) {
+  let arr = input.split(" ");
+  let big = arr[0];
+
+  console.log(`Input Array: ${arr}`);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > big) {
+      big = arr[i];
+    }
+  }
+  console.log("The largest number is ", big);
+}
+rl.question("Enter array elements seprated by space ", (input) => {
+  max(input);
+  rl.close();
+});
