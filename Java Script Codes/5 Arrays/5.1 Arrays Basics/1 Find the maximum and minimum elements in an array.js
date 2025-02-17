176ef156af07c8f2c8b908a67ac0b1c1d58f2c90
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-17 11:17:30 +0530

    printing minimum and maximum of array element using function

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/1 Find the maximum and minimum elements in an array.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/1 Find the maximum and minimum elements in an array.js
@@ -5,19 +5,28 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-function max(input) {
-  let arr = input.split(" ");
+function maxMin(input) {
+  let arr = input.trim().split(/\s+/).map(Number);
+  if (arr.some(isNaN)) {
+    console.log("Enter valid number ");
+    return;
+  }
   let big = arr[0];
+  let small = arr[0];
 
   console.log(`Input Array: ${arr}`);
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > big) {
       big = arr[i];
     }
+    if (arr[i] < small) {
+      small = arr[i];
+    }
   }
   console.log("The largest number is ", big);
+  console.log("The smallest number is ", small);
 }
 rl.question("Enter array elements seprated by space ", (input) => {
-  max(input);
+  maxMin(input);
   rl.close();
 });
