4a48fc029d07c8613aa9b08561b42b9a09268e6f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-18 14:55:35 +0530

    Reverse array

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/3 Reverse an array without using built-in methods.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/3 Reverse an array without using built-in methods.js
@@ -1,2 +1,25 @@
 // Reverse an array without using built-in methods.
 
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+function reverseArr(input){
+    console.log(`Original Array : ${input}`);
+    console.log("Revere array ");
+    for(let i=input.length-1;i>=0;i--){
+        console.log(input[i]);
+    }
+}
+rl.question("Enter the array elements by seprating then with space : ",(input)=>{
+    let arr=input.trim().split(" ").map(Number);
+    if(arr.every((num)=>!isNaN(num))){
+        reverseArr(arr);
+    }
+    else{
+        console.log("Enter valid array elements ");
+    }
+    rl.close();
+});
