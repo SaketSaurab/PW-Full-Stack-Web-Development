8205327f6149407c545179c9cc53f84a91a65aaa
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-18 08:58:00 +0530

    sum

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/2 Find the sum of all elements in an array.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/2 Find the sum of all elements in an array.js
@@ -1,1 +1,27 @@
 // Find the sum of all elements in an array.
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function sum(input) {
+  let num = input;
+  let sum = 0;
+  while (num > 0) {
+    sum = sum + (num % 10);
+    num =Math.floor(num/10);
+  }
+  console.log(`The sum of ${input} is ${sum}`);
+}
+
+rl.question("Enter the number ", (input) => {
+  if (/^\d+$/.test(input.trim())) {
+    let num = parseInt(input.trim(),10);
+    sum(num);
+  } else {
+    console.log("Enter valid number");
+  }
+  rl.close();
+});
