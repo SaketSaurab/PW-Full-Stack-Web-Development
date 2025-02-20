a8c96aa2261662a88a0ea8d2557a52146f8d54e2
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-20 13:27:51 +0530

    sorted array check program

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/6  Check if an array is sorted.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/6  Check if an array is sorted.js
@@ -6,7 +6,21 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function sortedArr(arr) {}
+function sortedArr(arr) {
+  let isSorted = true;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      isSorted = false;
+      break;
+    }
+  }
+  if (
+    isSorted
+      ? console.log("The given array is sorted ")
+      : console.log("The given array is not sorted")
+  );
+}
+
 rl.question("Enter the array elements ", (input) => {
   let arr = input.trim().split(" ").map(Number);
   if (arr.every((num) => !isNaN(num))) {
