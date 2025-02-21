e737c0d0555d01a7c48685ca401b8effbbc48176
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-21 18:50:24 +0530

    frequecny count

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/9 Find the frequency of each element in an array.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/9 Find the frequency of each element in an array.js
@@ -1,3 +1,25 @@
 // Find the frequency of each element in an array.
 
 const readline=require('readline');
+
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+function frequencyCount(arr){
+    let frequency={};
+    arr.forEach(element=>{
+        frequency[element]=(frequency[element]|| 0)+1;
+    });
+    console.log(frequency);
+}
+rl.question("Enter the array Elements : ",(input)=>{
+    let arr=input.trim().split(" ").map(Number);
+    if(arr.every((num)=>!isNaN(num))){
+        frequencyCount(arr);
+    }
+    else{
+        console.log("Enter valid array elements ");
+    }
+    rl.close();
+})
