c2595d3967f77e893cf6c3d620360c063192cca8
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-22 19:32:17 +0530

    rotation

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/11 Rotate an array by K positions.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/11 Rotate an array by K positions.js
@@ -1,1 +1,19 @@
 // Rotate an array by K positions.
+
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+function rotation(arr,k){
+
+}
+
+rl.question("Enter array : ",(input1)=>{
+    rl.question("Enter the number of rotation ",(input2)=>{
+        let arr=input1.trim().split(" ").map(Number);
+        let k=input2.trim();
+        
+    })
+})
