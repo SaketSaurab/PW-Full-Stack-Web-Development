7888c01552f19570c1811f5044cad6feb5f1d1d6
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-24 05:25:44 +0530

    Longest word in array

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/14 Find the longest word in an array.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/14 Find the longest word in an array.js
@@ -1,1 +1,23 @@
 // Find the longest word in an array.
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+function longWord(arr) {
+  let bigSize = arr[0].length;
+  let bigWord;
+  for (let i = 0; i < arr.length; i++) {
+    let currSize = arr[i].length;
+    if (currSize > bigSize) {
+      bigSize = currSize;
+      bigWord = arr[i];
+    }
+  }
+  console.log(`The biggest word in the array is ${bigWord}`);
+}
+rl.question("Enter the word array elements ", (input) => {
+  let arr = input.trim().split(" ");
+  longWord(arr);
+  rl.close();
+});
