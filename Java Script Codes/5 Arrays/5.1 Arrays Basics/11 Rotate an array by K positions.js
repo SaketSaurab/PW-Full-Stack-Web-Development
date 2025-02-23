b40299cab463f2bcd7af5d47b43fcdfa56977e97
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-23 21:41:26 +0530

    k rotation in array

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/11 Rotate an array by K positions.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/11 Rotate an array by K positions.js
@@ -1,19 +1,32 @@
 // Rotate an array by K positions.
 
-const readline=require('readline');
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-function rotation(arr,k){
-
+function rotation(arr, k) {
+  let n = arr.length;
+  k = k % n;
+  let newArr = [];
+  for (let i = 0; i < n; i++) {
+    let newIndex = (k + i) % n;
+    newArr[newIndex] = arr[i];
+  }
+  console.log("Original array : ", arr);
+  console.log(`Rotated array : ${newArr}`);
 }
 
-rl.question("Enter array : ",(input1)=>{
-    rl.question("Enter the number of rotation ",(input2)=>{
-        let arr=input1.trim().split(" ").map(Number);
-        let k=input2.trim();
-        
-    })
-})
+rl.question("Enter array : ", (input1) => {
+  rl.question("Enter the number of rotation ", (input2) => {
+    let arr = input1.trim().split(" ").map(Number);
+    let k = parseInt(input2.trim(),10);
+    if (arr.every(num=> !isNaN(num)) && /^\d+$/.test(k)){
+      rotation(arr, k);
+    } else {
+      console.log("Enter valid inputs");
+    }
+    rl.close();
+  });
+});
