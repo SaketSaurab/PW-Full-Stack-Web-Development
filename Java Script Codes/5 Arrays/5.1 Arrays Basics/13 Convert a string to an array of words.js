64cc17477371407fd342a800439d2773bdaadc79
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-23 22:25:34 +0530

    String to array

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/13 Convert a string to an array of words.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/13 Convert a string to an array of words.js
@@ -1,1 +1,20 @@
 // Convert a string to an array of words.
+
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+function strArray(str){
+let arr=[];
+for(let i=0;i<str.length;i++){
+    arr[i]=str.charAt(i);
+}
+console.log(`Original String : ${str}`);
+console.log(`Array : ${arr}`);
+}
+rl.question("Enter the string ",(input)=>{
+    let str=input.trim();
+    strArray(str);
+    rl.close();
+})
