ea9f709f98000f4db133beb9ac0f11b0a30d05b6
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-26 21:42:50 +0530

    sum of all even numbers in array

--- Java Script Codes/5 Arrays/5.1 Arrays Basics/18 Find the sum of all even numbers in an array.js
+++ Java Script Codes/5 Arrays/5.1 Arrays Basics/18 Find the sum of all even numbers in an array.js
@@ -1,2 +1,18 @@
 // Find the sum of all even numbers in an array.
 
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+function sumOfEven(arr){
+let evenArr=arr.filter(num=>num%2==0);
+let ans=evenArr.reduce((accumulator,currentValue)=>accumulator+currentValue,0);
+console.log(ans);
+}
+
+rl.question("Enter the array elements : ",(input1)=>{
+    let arr=input1.trim().split(" ").map(Number);
+    sumOfEven(arr);
+    rl.close();
+});
