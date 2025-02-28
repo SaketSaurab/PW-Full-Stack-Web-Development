54adc954c4c350786cb71f33e80b87f14c2a8368
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-02-28 19:08:25 +0530

    creating object and accessing its properties

--- Java Script Codes/6 Objects/6.1 Object Basics/1 Create an object and access its properties.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/1 Create an object and access its properties.js
@@ -1,1 +1,14 @@
 // Create an object and access its properties.
+
+const person={
+    name:"Ram",
+    age:25,
+    city:"Ayodhya"
+};
+console.log("Printing using for in loop");
+for(let key in person){
+console.log(`${key} : ${person[key]}`);
+}
+console.log(`person.name: ${person.name}`);
+console.log(`person.age: ${person.age}`);
+console.log(`person.city: ${person.city}`);
