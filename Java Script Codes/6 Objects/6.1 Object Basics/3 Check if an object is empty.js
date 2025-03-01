58633353d79143bd0645dedc18070cec3c3a5549
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-01 19:32:39 +0530

    using Object.keys

--- Java Script Codes/6 Objects/6.1 Object Basics/3 Check if an object is empty.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/3 Check if an object is empty.js
@@ -1,1 +1,7 @@
 // Check if an object is empty
+console.log("Method 1 using Object.keys");
+const obj1 = {};
+// const obj1={name:"sample"};
+Object.keys(obj1).length === 0
+  ? console.log("Object is empty")
+  : console.log("object is not empty");
