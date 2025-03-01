2cecbb36d328b3181c06e25c9893f61bb2c142ff
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-01 19:36:36 +0530

    using JSON.stringify(obj)==="{}"

--- Java Script Codes/6 Objects/6.1 Object Basics/3 Check if an object is empty.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/3 Check if an object is empty.js
@@ -1,7 +1,12 @@
 // Check if an object is empty
 console.log("Method 1 using Object.keys");
-const obj1 = {};
-// const obj1={name:"sample"};
-Object.keys(obj1).length === 0
+const obj = {};
+// const obj={name:"sample"};
+Object.keys(obj).length === 0
   ? console.log("Object is empty")
   : console.log("object is not empty");
+
+console.log("Method 2 using JSON.stringify()");
+JSON.stringify(obj) === "{}"
+  ? console.log("object is empty")
+  : console.log("Ojbect is not empty");
