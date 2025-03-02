0b23218223befbd2235955867d17dabad497135b
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-02 19:51:29 +0530

    method 2 using Object.entries(ob).length;

--- Java Script Codes/6 Objects/6.1 Object Basics/4 Find the number of properties in an object.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/4 Find the number of properties in an object.js
@@ -1,10 +1,14 @@
 // Find the number of properties in an object.
 
 const person = {
-    name: "Shyam",
-    age: 35,
-    city: "Mathura",
-  };
-  console.log("Using Object.keys(person).length");
-  let propertyCount1=Object.keys(person).length;
-  console.log(propertyCount1);
+  name: "Shyam",
+  age: 35,
+  city: "Mathura",
+};
+console.log("Using Object.keys(person).length");
+let propertyCount1 = Object.keys(person).length;
+console.log(propertyCount1);
+
+console.log("using Object.entries(persong).length");
+let propertyCount2 = Object.entries(person).length;
+console.log(propertyCount2);
