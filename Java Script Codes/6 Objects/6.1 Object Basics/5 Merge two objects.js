8f642590f0ff92088ff40c0e709ef971b0dcaab5
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-03 20:01:17 +0530

    merging objects

--- Java Script Codes/6 Objects/6.1 Object Basics/5 Merge two objects.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/5 Merge two objects.js
@@ -1,1 +1,17 @@
 // Merge two objects
+const obj1 = {
+  name: "Ram",
+  age: 28,
+};
+const obj2 = {
+  city: "Ayodhya",
+  brothes: "3",
+};
+
+console.log("Using Object.assign()");
+const mergeObj1 = Object.assign({}, obj1, obj2);
+console.log(mergeObj1);
+
+console.log("Using Spread operator ...");
+const mergeObj2 = { ...obj1, ...obj2 };
+console.log(mergeObj2);
