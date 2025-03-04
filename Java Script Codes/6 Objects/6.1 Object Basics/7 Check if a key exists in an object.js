d6dddaaaea9f936aa958b35df670dcfc676d2654
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-04 23:07:43 +0530

    key existing check

--- Java Script Codes/6 Objects/6.1 Object Basics/7 Check if a key exists in an object.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/7 Check if a key exists in an object.js
@@ -1,14 +1,18 @@
 // Check if a key exists in an object
-const people={
-    name:"Ram",
-    age:23,
-    city:"Ayodhya",
-    wife:"Seeta"
+const people = {
+  name: "Ram",
+  age: 23,
+  city: "Ayodhya",
+  wife: "Seeta",
 };
 console.log("Method 1 using operator");
-console.log("name"in people);
-console.log("father"in people);
+console.log("name" in people);
+console.log("father" in people);
 
 console.log("Method 2 using Object.hasOwnProperty()");
 console.log(people.hasOwnProperty("name"));
 console.log(people.hasOwnProperty("father"));
+
+console.log("Method 3 Object.hasOwn()"); // most useful
+console.log(Object.hasOwn(people, "name"));
+console.log(Object.hasOwn(people, "father"));
