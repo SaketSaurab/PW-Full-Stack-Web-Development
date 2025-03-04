3b843c63e792528be8e11bf03661650cd9c0b07f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-04 22:47:24 +0530

    Method 1 using JSON.parse(JSON.stringify(obj))

--- Java Script Codes/6 Objects/6.1 Object Basics/6 Clone an object without reference.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/6 Clone an object without reference.js
@@ -1,1 +1,6 @@
 // Clone an object without reference.
+const obj={name:"Rahul Dravid",nickName:"The wall"}
+
+console.log("Method 1 using JSON.parse(JSON.stringify(obj)");
+const cloneObj1=JSON.parse(JSON.stringify(obj));
+console.log(cloneObj1);
