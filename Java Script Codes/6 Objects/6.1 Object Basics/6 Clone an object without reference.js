430eae33409fd3d9d852ff3336b43231d48aae46
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-04 22:50:56 +0530

    method 3 using structuredClone

--- Java Script Codes/6 Objects/6.1 Object Basics/6 Clone an object without reference.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/6 Clone an object without reference.js
@@ -1,12 +1,14 @@
 // Clone an object without reference.
-const obj={name:"Rahul Dravid",nickName:"The wall"}
+const obj = { name: "Rahul Dravid", nickName: "The wall" };
 
 console.log("Method 1 using JSON.parse(JSON.stringify(obj)");
-const cloneObj1=JSON.parse(JSON.stringify(obj));
+const cloneObj1 = JSON.parse(JSON.stringify(obj));
 console.log(cloneObj1);
 
 console.log("Method 2 using ... spread operator");
-const cloneObj2={...obj};
+const cloneObj2 = { ...obj };
 console.log(cloneObj2);
 
-
+console.log("method 3 using structuredClone()");
+const cloneObj3 = structuredClone(obj);
+console.log(cloneObj3);
