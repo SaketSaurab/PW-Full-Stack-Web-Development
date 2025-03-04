60d652bfb235976e6ae2b52115155078a1198604
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-04 22:49:04 +0530

    method 2 using ... spread operator

--- Java Script Codes/6 Objects/6.1 Object Basics/6 Clone an object without reference.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/6 Clone an object without reference.js
@@ -4,3 +4,9 @@ const obj={name:"Rahul Dravid",nickName:"The wall"}
 console.log("Method 1 using JSON.parse(JSON.stringify(obj)");
 const cloneObj1=JSON.parse(JSON.stringify(obj));
 console.log(cloneObj1);
+
+console.log("Method 2 using ... spread operator");
+const cloneObj2={...obj};
+console.log(cloneObj2);
+
+
