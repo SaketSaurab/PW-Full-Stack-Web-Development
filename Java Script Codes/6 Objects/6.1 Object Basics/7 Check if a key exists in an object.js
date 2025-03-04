9aa3286d4f45911c70e8ab8f0c0a6aae7ed29913
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-04 23:05:23 +0530

    method 2 using  obj.hasOwnProperty()

--- Java Script Codes/6 Objects/6.1 Object Basics/7 Check if a key exists in an object.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/7 Check if a key exists in an object.js
@@ -8,3 +8,7 @@ const people={
 console.log("Method 1 using operator");
 console.log("name"in people);
 console.log("father"in people);
+
+console.log("Method 2 using Object.hasOwnProperty()");
+console.log(people.hasOwnProperty("name"));
+console.log(people.hasOwnProperty("father"));
