85914dcf48215b2a047809b636d8447af91ee4f9
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-07 22:32:21 +0530

    finding all keys and values of an object

--- Java Script Codes/6 Objects/6.1 Object Basics/11 Find all keys and values of an object.js
+++ Java Script Codes/6 Objects/6.1 Object Basics/11 Find all keys and values of an object.js
@@ -1,1 +1,21 @@
 // Find all keys and values of an object.
+const person={
+    name:"Ram",
+    age:23,
+    city:"Ayodhya"
+}
+
+// method 1 using object.keys() and object.values();
+console.log("Method 1");
+ console.log(`Keys : ${Object.keys(person)}`);
+ console.log(`Values: ${Object.values(person)}`);
+
+ // method 2 using Object.entries()
+console.log("Method 2");
+ console.log(`Entries: ${Object.entries(person)}`);
+
+ // method 3 using for ..in loop
+ console.log("Method 3 using for in loop");
+ for(let key in person){
+    console.log(`Key: ${key} Values: ${person[key]}`);
+ }
