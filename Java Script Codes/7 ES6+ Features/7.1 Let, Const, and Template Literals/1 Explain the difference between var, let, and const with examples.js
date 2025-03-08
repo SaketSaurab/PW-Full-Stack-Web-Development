d941b13f8162907c2e33536a396413d1a0830ac0
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-08 16:55:24 +0530

    var let const

---
+++ Java Script Codes/7 ES6+ Features/7.1 Let, Const, and Template Literals/1 Explain the difference between var, let, and const with examples.js
@@ -0,0 +1,6 @@
+// Explain the difference between var, let, and const with examples.
+/*
+var: It is function scope,and can be re-declared and updated
+let: It is block scope and can be updated not re-declared
+const: It is block scope and can not be re-declared or updated
+*/
