d7164aa3c693c8cbfa5c7877a27eb9c9a122eeb5
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-10 17:06:50 +0530

    react app 2

---
+++ React/react-vite-2/react-app-2/src/assets/components/UserCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./UserCard.css"
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+    <p id='user-name'> User name : Love Babbar</p>
+    <img id ='user-img'src='https://imgs.search.brave.com/UT9DTxBrnGs71p_Nky5fRUq54iiahVWXgexhno5L0yo/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9ncmFw/aC5mYWNlYm9vay5j/b20vMTUyOTMzMTcy/Mzg0MTIzMC9waWN0/dXJlP3R5cGU9bGFy/Z2U.jpeg' alt='love'></img>
+    <p id='user-desc'>Description of Love Babbar</p>
+      
+    </div>
+  )
+}
+
+export default UserCard
