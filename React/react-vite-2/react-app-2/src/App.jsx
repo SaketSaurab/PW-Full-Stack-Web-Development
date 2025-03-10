1dc6f378101b0406332c4bcacfabed0fadcaf84b
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-10 17:11:59 +0530

    adding props

--- React/react-vite-2/react-app-2/src/App.jsx
+++ React/react-vite-2/react-app-2/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
     <div className="container">
-    <UserCard/>
-    <UserCard/>
-    <UserCard/>
+    <UserCard name="Love Rana"/>
+    <UserCard name="Maharana Pratap"/>
+    <UserCard name="PrithviRaj Chauhan"/>
       
     </div>
   )
--- React/react-vite-2/react-app-2/src/assets/components/UserCard.jsx
+++ React/react-vite-2/react-app-2/src/assets/components/UserCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./UserCard.css"
-const UserCard = () => {
+const UserCard = (props) => {
   return (
     <div className='user-container'>
-    <p id='user-name'> User name : Love Babbar</p>
+    <p id='user-name'> {props.name}</p>
     <img id ='user-img'src='https://imgs.search.brave.com/UT9DTxBrnGs71p_Nky5fRUq54iiahVWXgexhno5L0yo/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9ncmFw/aC5mYWNlYm9vay5j/b20vMTUyOTMzMTcy/Mzg0MTIzMC9waWN0/dXJlP3R5cGU9bGFy/Z2U.jpeg' alt='love'></img>
     <p id='user-desc'>Description of Love Babbar</p>
       
