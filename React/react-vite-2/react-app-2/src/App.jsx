1873b9e5f0ea0c7964969dc60618eff9d8ed533e
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-11 14:39:15 +0530

    react app 2

--- React/react-vite-2/react-app-2/src/App.jsx
+++ React/react-vite-2/react-app-2/src/App.jsx
@@ -1,13 +1,16 @@
 import UserCard from "./assets/components/UserCard"
 import "./app.css"
+import Love from "./assets/Love.jpg"
+import maharana from "./assets/MaharanaPratap.jpg"
+import prithvi from "./assets/PrithviRajChauhan.jpg"
 
 function App() {
 
   return (
     <div className="container">
-    <UserCard name="Love Rana"/>
-    <UserCard name="Maharana Pratap"/>
-    <UserCard name="PrithviRaj Chauhan"/>
+    <UserCard name="Love Rana" desc="Description one" image={Love} style={{"border-radius":"10px"}}/>
+    <UserCard name="Maharana Pratap" desc="Description two" image={maharana} style={{"border-radius":"10px"}}/>
+    <UserCard name="PrithviRaj Chauhan" desc="Description three" image={prithvi} style={{"border-radius":"10px"}}/>
       
     </div>
   )
--- React/react-vite-2/react-app-2/src/assets/components/UserCard.jsx
+++ React/react-vite-2/react-app-2/src/assets/components/UserCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import "./UserCard.css"
 const UserCard = (props) => {
   return (
-    <div className='user-container'>
+    <div className='user-container' style={props.style}>
     <p id='user-name'> {props.name}</p>
-    <img id ='user-img'src='https://imgs.search.brave.com/UT9DTxBrnGs71p_Nky5fRUq54iiahVWXgexhno5L0yo/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9ncmFw/aC5mYWNlYm9vay5j/b20vMTUyOTMzMTcy/Mzg0MTIzMC9waWN0/dXJlP3R5cGU9bGFy/Z2U.jpeg' alt='love'></img>
-    <p id='user-desc'>Description of Love Babbar</p>
+    <img id ='user-img'src={props.image} alt={props.name}></img>
+    <p id='user-desc'>{props.desc}</p>
       
     </div>
   )
