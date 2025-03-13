353acc1154f9b2305b3c0515d3bb7d921bc9e1b2
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-13 20:17:31 +0530

    passing props

--- React/react-vite-4/passing-props/src/App.jsx
+++ React/react-vite-4/passing-props/src/App.jsx
@@ -1,11 +1,31 @@
 
+import { useState } from 'react'
 import './App.css'
+import Card from './assets/Components/Card'
+import Button from './assets/Components/Button';
 
 function App() {
 
+  const [count,setCount]=useState(0);
+  function hadleClick(){
+    setCount(count+1);
+  }
+
   return (
     <div>
-     
+  <Button handleClick={hadleClick} text="Click me">
+<h1>{count}</h1>
+</Button>
+
+     {/* <Card name="Love babbar">
+      <h1>Best Web Dev</h1>
+      <p>Trying to be consistance</p>
+      <p>Will complete the course soon</p>
+
+     </Card>
+     <Card children="mai bhi children hu">
+
+     </Card> */}
       
     </div>
   )
