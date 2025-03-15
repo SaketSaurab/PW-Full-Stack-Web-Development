a3bd4291c0a87138b0b803d9ac9c5630b96521b1
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-15 17:23:28 +0530

    event handling

--- React/react-vite-7/Event-Handling/src/App.jsx
+++ React/react-vite-7/Event-Handling/src/App.jsx
@@ -4,23 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  function handleClick(){
-alert("I am clicked");
+  function handleClick() {
+    alert("I am clicked");
   }
- function paraMouse(){
-  alert("Para ke upar mouse leke aaye ho ");
- }
-function inputChange(e){
-console.log("Input change hua hai ",e.target.value);
-  console.log("Input me value change hua hai");
-}
+
+  function paraMouse() {
+    alert("Para ke upar mouse leke aaye ho");
+  }
+
+  function inputChange(e) {
+    console.log("Input change hua hai ", e.target.value);
+    console.log("Input me value change hua hai");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // i am writting my custom behaviour down
+    alert("Form submit kar du kya");
+  }
+
   return (
-    <div >
-    <input type='text' onChange={inputChange}/>
-      <button onClick={handleClick} >Click me</button>
-      {/* <p onMouseOver={paraMouse}>para hai ye </p> */}
+    <div>
+      <button onClick={() => alert("Button click hua hai")}>click me here</button>
+
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={inputChange} />
+        <button onClick={handleClick}>Click me</button>
+        <button type='submit'>Submit</button>
+        <p onMouseOver={paraMouse}>para hai ye</p>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
