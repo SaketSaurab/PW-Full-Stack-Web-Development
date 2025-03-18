4106e2d21590546965cf96c996a1ea3c9df7637a
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-18 18:30:44 +0530

    useEffecct with five types of variations

--- React/react-vite-8/use-effect/src/App.jsx
+++ React/react-vite-8/use-effect/src/App.jsx
@@ -1,13 +1,78 @@
 
 import './App.css'
-
+import { useEffect , useState} from 'react'
 function App() {
- 
+ const [count,setCount]=useState(0);
+const [total,setTotal]=useState(1);
+// first -> side effect function
+// second-> clean up function
+// third -> comma seprated  dep list
+
+// variation 1 : 
+// runs on every render
+// useEffect(()=>{
+  //  alert("I will render on each render")})
+//--------------------------------
+
+
+  // variation 2 
+  // that runs only on first render 
+  // 
+
+
+// useEffect(() => {
+//  alert("I will run only on first render");
+// }, [])
+//------------------------------------
+// variation 3: 
+
+// useEffect(() => {
+// alert("I will run each time when count is updated");
+// }, [count]);
+//-------------------
+
+// variation 4
+// multiple dependencies
+
+
+// useEffect(() => {
+// alert("I will run every time when count or total is updated");
+// }, [count,total]);
+//------------------
+
+
+// variation 5 
+/// is baar lets add cleanup function
+// useEffect(() => {
+//   alert("The count is updated")
+
+//   return () => {
+//     alert("The count is unmounted from ui");
+//   }
+// }, [count]);
+ //--------------
+
+
+
+function handleClick(){
+  setCount(count+1);
+}
+
+function handleClickTotal(){
+  
+  setTotal(total+1);
+}
 
   return (
     <div>
     <p><b>Hello JI</b></p>
-      use effect
+    <button onClick={handleClick}>click here</button>
+    <br/>
+     Count is : {count}
+     <br/>
+       <button onClick={handleClickTotal}> Total</button>
+    <br/>
+     Total is : {total}
     </div>
   )
 }
