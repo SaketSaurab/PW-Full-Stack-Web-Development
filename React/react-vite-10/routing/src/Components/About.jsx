2c3a32f0346e7122ae889fae9e20b44884082a25
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-20 18:41:47 +0530

    UseNavigate

--- React/react-vite-10/routing/src/Components/About.jsx
+++ React/react-vite-10/routing/src/Components/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const about = () => {
+  const navigate=useNavigate();
+  function handleClick(){
+    navigate('/Dashboard');
+  }
   return (
     <div>
       About Page
+      <button onClick={handleClick}>Move to Dashboard</button>
     </div>
   )
 }
--- React/react-vite-10/routing/src/Components/Home.jsx
+++ React/react-vite-10/routing/src/Components/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const home = () => {
+  const navigate=useNavigate();
+  function handleClick(){
+    navigate('/About');
+  }
   return (
     <div>
       Home Page
+      <button onClick={handleClick}>Move to about</button>
     </div>
   )
 }
