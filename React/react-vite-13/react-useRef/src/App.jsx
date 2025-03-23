07d3fbd641c455f8a7fb193938a309bd0fc4b468
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-23 15:32:39 +0530

    react useRef

--- React/react-vite-13/react-useRef/src/App.jsx
+++ React/react-vite-13/react-useRef/src/App.jsx
@@ -3,12 +3,16 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-let val=useRef(0);
-
+  let val = useRef(0);
+let btnRef=useRef();
   function handleIncrement() {
-    val.current=val.current+1;
+    val.current = val.current + 1;
     console.log(`value of val : ${val.current}`);
     setCount(count + 1);
+  }
+
+  function changeColour() {
+    btnRef.current.style.backgroundColor="red";
   }
 
   // it runs on every render
@@ -18,9 +22,14 @@ let val=useRef(0);
 
   return (
     <div>
-      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleIncrement}
+      ref={btnRef}
+      >Increment</button>
       <br />
-
+      <br />
+      <button onClick={changeColour}>Change colour of first button</button>
+      <br />
+      <br />
       <div>count:{count}</div>
     </div>
   );
