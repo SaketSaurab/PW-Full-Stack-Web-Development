8d63b4c7e601836a0c3e57805f79db0549773c1f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-25 22:31:56 +0530

    expensive task creation

--- React/react-vite-14/use-memo-project/src/App.jsx
+++ React/react-vite-14/use-memo-project/src/App.jsx
@@ -1,30 +1,29 @@
-
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
- const [count,setCount]=useState(0);
- function expensiveTask(num){
-  console.log("Inside Expensive task");
-  for(let i=0;i<1000000000;i++){}
-  return num*2;
- }
- let doubleValue=expensiveTask(4);
+  const [count, setCount] = useState(0);
+  const[input,setInput]=useState(0);
+  function expensiveTask(num) {
+    console.log("Inside Expensive task");
+    for (let i = 0; i < 1000000000; i++) {}
+    return num * 2;
+  }
+  let doubleValue = expensiveTask(input);
 
   return (
     <div>
-     <button onClick={()=>setCount(count+1)}>
-      Increment
-     </button>
-     <div>
-      Count:{count}
-     </div>
-     <div>
-      Double: {doubleValue }
-     </div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <div>Count:{count}</div>
+      <input 
+        type="number"
+        placeholder="Enter Number"
+        value={input}
+        onChange={(e)=>setInput(e.target.value)}
+      />
+      <div>Double: {doubleValue}</div>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
