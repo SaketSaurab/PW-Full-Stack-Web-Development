e1ca3872ccfd8198cd279b33f08146d99cdb5f9c
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-03-26 01:17:32 +0530

    using use call back

--- React/react-vite-15/use-callback-project/src/Components/ExpensiveComponents.jsx
+++ React/react-vite-15/use-callback-project/src/Components/ExpensiveComponents.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 
 const ExpensiveComponents = () => {
     const[count,setCount]=useState(0);
     const[text,setText]=useState("");
+    const previousFunction=useRef(null);
 
-    const ExpensiveCalculation=()=>{
+    const ExpensiveCalculation=useCallback(()=>{
         console.log("Running expensive calculation..");
         let result=0;
         for(let i=0;i<1000000000;i++){
             result+=i;
         }
         return result;
-    }
+    },[count]);
 
+
+    useEffect(() => {
+
+        if(previousFunction.current){
+                if(previousFunction.current===ExpensiveCalculation){
+console.log("Function not re created");
+        }
+        else{
+            console.log("Function got re created");
+        }
+        }
+        else{
+            previousFunction.current=ExpensiveCalculation;
+        }
+    }, [ExpensiveCalculation])
+    
 
 
   return (
