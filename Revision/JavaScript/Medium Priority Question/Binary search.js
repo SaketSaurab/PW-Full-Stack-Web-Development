83332a6c355490352a6b4793dfa2577333fe3d68
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-07 14:00:02 +0530

    Binary searching

--- Revision/JavaScript/Medium Priority Question/Binary search.js
+++ Revision/JavaScript/Medium Priority Question/Binary search.js
@@ -1,26 +1,40 @@
 // binary search
-const readline=require('readline');
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-
-function binarySearch(input,element){
-    
-    let arr=input.trim().split(/\s+/).map(Number);
-    let e=parseInt(element);
-    if(arr.some(isNaN)|| isNaN(e)){
-        console.log("Please enter valid input");
-        rl.close();
-        return;
+function binarySearch(input, element) {
+  let arr = input.trim().split(/\s+/).map(Number);
+  let e = parseInt(element);
+  if (arr.some(isNaN) || isNaN(e)) {
+    console.log("Please enter valid input");
+    rl.close();
+    return;
+  }
+  console.log("Input array : ", arr);
+//   arr=arr.sort((a,b)=>a-b);                  // sorting method
+  let first = 0;
+  let last = arr.length - 1;
+  while (first <= last) {
+    let mid = Math.floor((first + last) / 2);
+    if (arr[mid] === e) {
+      console.log(`${e} is found at index ${mid}`);
+      rl.close();
+      return;
+    } else if (arr[mid] < e) {
+      first = mid + 1;
+    } else if (arr[mid] > e) {
+      last = mid - 1;
     }
-    console.log("Input array : ",arr);
-    
+  }
+  console.log(`${e}  is not present in the given array    `);
+  rl.close();
 }
 
-rl.question("Enter array element in sorted order: ",(input1)=>{
-    rl.question("Enter the element to search : ",(input2)=>{
-        binarySearch(input1,input2);
-    });
+rl.question("Enter array element in sorted order: ", (input1) => {
+  rl.question("Enter the element to search : ", (input2) => {
+    binarySearch(input1, input2);
+  });
 });
