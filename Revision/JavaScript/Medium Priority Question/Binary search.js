850b0c2f90c9e7f46f8a9f18adf919d53e79944b
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-07 19:59:25 +0530

    binary search

--- Revision/JavaScript/Medium Priority Question/Binary search.js
+++ Revision/JavaScript/Medium Priority Question/Binary search.js
@@ -1,1 +1,26 @@
 // binary search
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+
+function binarySearch(input,element){
+    
+    let arr=input.trim().split(/\s+/).map(Number);
+    let e=parseInt(element);
+    if(arr.some(isNaN)|| isNaN(e)){
+        console.log("Please enter valid input");
+        rl.close();
+        return;
+    }
+    console.log("Input array : ",arr);
+    
+}
+
+rl.question("Enter array element in sorted order: ",(input1)=>{
+    rl.question("Enter the element to search : ",(input2)=>{
+        binarySearch(input1,input2);
+    });
+});
