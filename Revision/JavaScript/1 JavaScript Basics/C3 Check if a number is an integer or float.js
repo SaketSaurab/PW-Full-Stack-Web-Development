65b9a40325d8ca704faa2bc59d3c70f4d32a30cc
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-08 15:24:47 +0530

    Number check

--- Revision/JavaScript/1 JavaScript Basics/C3 Check if a number is an integer or float.js
+++ Revision/JavaScript/1 JavaScript Basics/C3 Check if a number is an integer or float.js
@@ -1,1 +1,24 @@
 // Check if a number is an integer or float
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+function check(input) {
+  input = input.trim();
+  const intRegex = /^-?\d+$/;
+  const floatRegex = /^-?\d+\.\d+$/;
+
+  if (intRegex.test(input)) {
+    console.log(`${input} is integer`);
+  } else if (floatRegex.test(input)) {
+    console.log(`${input} is float`);
+  } else {
+    console.log(`${input} is not number`);
+  }
+}
+
+rl.question("Enter the number to check ", (input) => {
+  check(input);
+  rl.close();
+});
