6b9e0e2ef55df509f36c517029766718ff8eb69b
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-09 17:27:34 +0530

    fixed decimal format

--- Revision/JavaScript/1 JavaScript Basics/C4 Convert a number to a fixed decimal format.js
+++ Revision/JavaScript/1 JavaScript Basics/C4 Convert a number to a fixed decimal format.js
@@ -1,1 +1,23 @@
 // Convert a number to a fixed decimal format
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function decimalFormat(input) {
+  input = input.trim();
+  let numCheck = /^-?\d+\.\d+$/;
+  if (numCheck.test(input)) {
+    let number = parseFloat(input);
+    let fixed = parseFloat(number.toFixed(2));
+    console.log(fixed);
+  } else {
+    console.log("Invalid input");
+  }
+}
+
+rl.question("Enter number ", (input) => {
+  decimalFormat(input);
+  rl.close();
+});
