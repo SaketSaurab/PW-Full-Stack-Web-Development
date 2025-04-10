78b8b3baf40a4b0e1da2ea992ad1aa4114ec31ae
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-10 17:52:48 +0530

    string contains number only check

--- Revision/JavaScript/1 JavaScript Basics/C5 Check if a string contains only numbers.js
+++ Revision/JavaScript/1 JavaScript Basics/C5 Check if a string contains only numbers.js
@@ -1,1 +1,21 @@
 // Check if a string contains only numbers
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function check(input) {
+  input = input.trim();
+  let num = /^-?\d+(\.\d+)?$/;
+  if (num.test(input)) {
+    console.log(`${input} contains only numbers`);
+  } else {
+    console.log(`${input} contains string also`);
+  }
+}
+
+rl.question("Enter the input", (input) => {
+  check(input);
+  rl.close();
+});
