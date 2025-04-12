769f6b15bb4cdd647e2423a0fd49df8ff535e262
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-12 19:37:13 +0530

    nullish coalescing operator

--- Revision/JavaScript/2 Operators and Expressions/C3  Use the nullish coalescing operator to assign default values.js
+++ Revision/JavaScript/2 Operators and Expressions/C3  Use the nullish coalescing operator to assign default values.js
@@ -1,1 +1,18 @@
 //  Use the nullish coalescing operator to assign default values
+
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+function fun(input){
+    const value=input?? "default value";
+    console.log(`Final value : ${value}`);
+}
+
+rl.question("Enter value : ",(input)=>{
+    newInput=input===""?null:input;
+    fun(newInput);
+    rl.close();
+});
