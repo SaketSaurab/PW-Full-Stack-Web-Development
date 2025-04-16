01aedd362f873b23b09a991e1a36c6499fa67f6f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-16 22:32:14 +0530

    reverse string

--- Revision/JavaScript/4 Loops/C5 Print the reverse of a string.js
+++ Revision/JavaScript/4 Loops/C5 Print the reverse of a string.js
@@ -1,1 +1,20 @@
 // Print the reverse of a string
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function reverseString(input) {
+  let len = input.length;
+  let rev = [];
+  for (let i = len - 1; i >= 0; i--) {
+    rev.push(input.charAt(i));
+  }
+  console.log("Reverse string : ", rev.join(""));
+}
+
+rl.question("Enter string ", (input) => {
+  reverseString(input);
+  rl.close();
+});
