c6413909f7cc12da527f3d2471d610fccb468d94
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-18 19:45:58 +0530

    function to capitalize the first letter of string

--- Revision/JavaScript/5 Function/C3 Write a function to capitalize the first letter of each word in a string.js
+++ Revision/JavaScript/5 Function/C3 Write a function to capitalize the first letter of each word in a string.js
@@ -1,2 +1,19 @@
-
 // Write a function to capitalize the first letter of each word in a string
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+function firstLetter(input) {
+  let first = input
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+  console.log(`Input string : ${input}`);
+  console.log(`Capitalize stirng : ${first}`);
+}
+
+rl.question("Enter the string : ", (input) => {
+  firstLetter(input);
+  rl.close();
+});
