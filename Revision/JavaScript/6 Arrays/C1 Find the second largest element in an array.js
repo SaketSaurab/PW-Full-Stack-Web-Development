580f8e17ce0960df5eaeab28cd2ef8c6606337f5
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-19 20:39:24 +0530

    Second largest element

--- Revision/JavaScript/6 Arrays/C1 Find the second largest element in an array.js
+++ Revision/JavaScript/6 Arrays/C1 Find the second largest element in an array.js
@@ -1,1 +1,34 @@
 // Find the second largest element in an array
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function secondLargestElement(input) {
+  let arr = input.split(" ").map(Number);
+  let first = -Infinity;
+  let second = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > first) {
+      second = first;
+      first = arr[i];
+    } else if (arr[i] > second && arr[i] < first) {
+      second = arr[i];
+    }
+  }
+
+  if (second === -Infinity) {
+    console.log("Second largest does not exist");
+  } else {
+    console.log("Second largest element is", second);
+    console.log(`Biggest number: ${first}`);
+  }
+}
+
+rl.question("Enter the array elements : ", (input) => {
+  secondLargestElement(input);
+  rl.close();
+});
