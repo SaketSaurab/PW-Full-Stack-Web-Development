00edd40d943ac5767b2e71285d5bc5215242b074
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-20 15:30:59 +0530

    Lognest word in string

--- Revision/JavaScript/6 Arrays/C3 Find the longest word in an array of strings.js
+++ Revision/JavaScript/6 Arrays/C3 Find the longest word in an array of strings.js
@@ -1,1 +1,24 @@
 // Find the longest word in an array of strings
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function findLongestWord(input) {
+  let inputArr = input.split(" ");
+  let big = "";
+  let bigLen = 0;
+  for (let i = 0; i < inputArr.length; i++) {
+    if (inputArr[i].length > bigLen) {
+      bigLen = inputArr[i].length;
+      big = inputArr[i];
+    }
+  }
+  console.log("Longest word in string is ", big);
+}
+
+rl.question("Enter the string : ", (input) => {
+  findLongestWord(input);
+  rl.close();
+});
