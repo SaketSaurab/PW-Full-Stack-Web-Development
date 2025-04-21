4c10b34dddb03e42b70d2e0bdaa2de5531c2ac4c
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-21 16:07:49 +0530

    finding missing positive interger

--- Revision/JavaScript/6 Arrays/C4 Find the smallest missing positive integer in an array.js
+++ Revision/JavaScript/6 Arrays/C4 Find the smallest missing positive integer in an array.js
@@ -1,1 +1,27 @@
 // Find the smallest missing positive integer in an array
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function missing(input) {
+  let positive = input.filter((num) => num > 0);
+  positive.sort((a, b) => a - b);
+
+  let smallest = 1;
+  for (let i = 0; i < positive.length; i++) {
+    if (positive[i] === smallest) {
+      smallest++;
+    } else if (positive[i] > smallest) {
+      break;
+    }
+  }
+
+  console.log("Smallest missing integer : ", smallest);
+}
+rl.question("Enter array : ", (input) => {
+    let arr=input.split(" ").map(Number);
+  missing(arr);
+  rl.close();
+});
