2adf0ad5aac31f749fa32ab3d404c3cc3e5cfdc0
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-22 18:41:36 +0530

    Binary search

--- Revision/JavaScript/6 Arrays/C6 Implement binary search.js
+++ Revision/JavaScript/6 Arrays/C6 Implement binary search.js
@@ -1,25 +1,37 @@
 // Implement binary search
-const readline=require('readline');
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stout
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-function binarySearch(input,e){
+function binarySearch(input, e) {
+  let left = 0;
+  let right = input.length - 1;
 
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (input[mid] == e) {
+      return console.log(`element ${e} is present at index ${mid}`);
+    } else if (input[mid] < e) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  console.log(`Element ${e} is not found `);
 }
 
-
-rl.question("Input the array : ",(input)=>{
-    rl.question("Enter the element to search : ",(e)=>{
-        let arr=input.split(" ").map(Number);
-        let a=e.map(Number);
-        if(!arr.every(num=>!isNaN(num)) || /^\d+$/.test(a.trim)){
-            binarySearch(arr,a);
-            rl.close();
-        }
-        else{
-            console.log("Enter valid input");
-        }
-    });
+rl.question("Input the array : ", (input) => {
+  rl.question("Enter the element to search : ", (e) => {
+    let arr = input.split(" ").map(Number);
+    let a = Number(e);
+    if (arr.some(isNaN) || isNaN(a)) {
+      console.log("Enter valid input");
+      rl.close();
+    } else {
+      binarySearch(arr, a);
+      rl.close();
+    }
+  });
 });
