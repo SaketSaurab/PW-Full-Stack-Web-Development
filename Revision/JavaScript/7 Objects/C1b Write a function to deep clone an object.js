3e8985c878facbf2d445dbb1b37a1d3527da7acd
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-25 17:26:21 +0530

    deep copy

--- Revision/JavaScript/7 Objects/C1b Write a function to deep clone an object.js
+++ Revision/JavaScript/7 Objects/C1b Write a function to deep clone an object.js
@@ -1,1 +1,12 @@
 // Write a function to deep clone an object
+
+function deepClone(obj){
+    return JSON.parse(JSON.stringify(obj));
+}
+const original={name:"bob",address:{city:"new York"}}
+const copy=deepClone(original);
+
+
+copy.address.city="New Delhi";
+console.log("Original : ", original);
+console.log("Copy : ",copy);
