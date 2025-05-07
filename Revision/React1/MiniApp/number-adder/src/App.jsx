de347b2a5298299db343ee3cdee2576e6f106b85
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-07 19:27:33 +0530

    sum of four numbers

--- Revision/React1/MiniApp/number-adder/src/App.jsx
+++ Revision/React1/MiniApp/number-adder/src/App.jsx
@@ -4,15 +4,20 @@ import "./App.css";
 function App() {
   const [num1, setNum1] = useState("");
   const [num2, setNum2] = useState("");
+
+  const [num3, setNum3] = useState("");
+  const [num4, setNum4] = useState("");
   const [result, setResult] = useState("");
 
   const handleAddition = () => {
-    const sum = Number(num1) + Number(num2);
+    const sum = Number(num1) + Number(num2) + Number(num3) + Number(num4);
     setResult(sum);
   };
   const handleReset = () => {
     setNum1("");
     setNum2("");
+    setNum3("");
+    setNum4("");
     setResult(null);
   };
 
@@ -33,6 +38,24 @@ function App() {
               placeholder="Enter second number"
               value={num2}
               onChange={(e) => setNum2(e.target.value)}
+            />
+          </div>
+
+          <div className="div-input"> + </div>
+          <div className="div-input">
+            <input
+              placeholder="Enter third number"
+              value={num3}
+              onChange={(e) => setNum3(e.target.value)}
+            />
+          </div>
+
+          <div className="div-input"> + </div>
+          <div className="div-input">
+            <input
+              placeholder="Enter fourth number"
+              value={num4}
+              onChange={(e) => setNum4(e.target.value)}
             />
           </div>
           <div className="div-input"> = {result} </div>
