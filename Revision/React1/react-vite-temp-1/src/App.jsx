552a9c0e856d546d0a79a9793ad862bcf0db1678
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-07 21:05:49 +0530

    react revision 1

---
+++ Revision/React1/react-vite-temp-1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css';
+
+function App() {
+
+
+  return (
+    <>
+      <p>Hello ji Kaise ho</p>
+    </>
+  )
+}
+
+export default App
---
+++ Revision/React1/react-vite-temp-1/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      Mai navbar hu
+    </div>
+  )
+}
+
+export default navbar
