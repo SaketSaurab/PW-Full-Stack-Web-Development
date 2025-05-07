ccb456db0bf9dc95eb9b29528bda00d3f0f22377
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-07 19:14:59 +0530

    Addition working

--- Revision/React1/MiniApp/number-adder/src/App.jsx
+++ Revision/React1/MiniApp/number-adder/src/App.jsx
@@ -1,19 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState("");
 
+  const handleAddition = () => {
+    const sum = Number(num1) + Number(num2);
+    setResult(sum);
+  };
+  const handleReset = () => {
+    setNum1("");
+    setNum2("");
+    setResult(null);
+  };
 
   return (
     <>
-      <div>
-        <h2>Hello</h2>
+      <div className="div-container div">
+        <div className="div-math">
+          <div className="div-input">
+            <input
+              placeholder="Enter First number"
+              value={num1}
+              onChange={(e) => setNum1(e.target.value)}
+            />
+          </div>
+          <div className="div-input"> + </div>
+          <div className="div-input">
+            <input
+              placeholder="Enter second number"
+              value={num2}
+              onChange={(e) => setNum2(e.target.value)}
+            />
+          </div>
+          <div className="div-input"> = {result} </div>
+        </div>
+
+        <div className="div-button">
+          <button onClick={handleAddition}>Submit</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
       </div>
-       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
