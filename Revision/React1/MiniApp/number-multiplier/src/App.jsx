7c511431fd1bd711d96ac04b199ca0a30bdace4c
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-08 20:01:20 +0530

    multiplication function added

--- Revision/React1/MiniApp/number-multiplier/src/App.jsx
+++ Revision/React1/MiniApp/number-multiplier/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState(null);
+  const handleMultiply = () => {
+    const ans = Number(num1) * Number(num2);
+    setResult(ans);
+  };
+  const handleReset=()=>{
+    setNum1('');
+    setNum2('');
+    setResult(null);
+  }
   return (
     <>
-      <div>
-        hello
+      <div className="div-container">
+        <div className="div-input"></div>
+
+        <input
+          placeholder="First Number"
+          value={num1}
+          onChange={(e) => setNum1(e.target.value)}
+        />
+        <input
+          placeholder="Second Number"
+          value={num2}
+          onChange={(e) => setNum2(e.target.value)}
+        />
+        <h3> = {result}</h3>
+
+        <div className="div-button">
+          <button onClick={handleReset}>Reset</button>
+          <button onClick={handleMultiply}>Calculate</button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
