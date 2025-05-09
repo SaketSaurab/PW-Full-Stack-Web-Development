b19eb2a266077c201094bc27af8027d3909bf608
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-09 15:58:55 +0530

    logic updated

--- Revision/React1/MiniApp/temprature-converter/src/App.jsx
+++ Revision/React1/MiniApp/temprature-converter/src/App.jsx
@@ -4,20 +4,34 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [temp, setTemp] = useState("");
+  const [result, setResult] = useState(null);
 
+  const calculateTemp = () => {
+    let ans = temp * (9 / 5) + 32;
+    setResult(ans);
+  };
 
+  const handleReset=()=>{
+    setTemp('');
+    setResult(null);
+  }
 
   return (
     <>
       <div className="div-container">
         <div className="div-input-output">
-          <input placeholder="Enter temprature in Celsius" />
-          <span> = Ans</span>
+          <input
+            placeholder="Enter temprature in Celsius"
+            value={temp}
+            onChange={(e) => setTemp(e.target.value)}
+          />
+          <span> = {result}</span>
         </div>
 
         <div className="div-button">
-          <button >Reset</button>
-          <button>Convert</button>
+          <button onClick={handleReset}>Reset</button>
+          <button onClick={calculateTemp}>Convert</button>
         </div>
       </div>
     </>
