5894ea0fa121fa132865d45a70f2460ef23da461
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-09 15:37:37 +0530

    UI updated

--- Revision/React1/MiniApp/temprature-converter/src/App.jsx
+++ Revision/React1/MiniApp/temprature-converter/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
 
+
+
   return (
     <>
-     <div>Hello</div>
+      <div className="div-container">
+        <div className="div-input-output">
+          <input placeholder="Enter temprature in Celsius" />
+          <span> = Ans</span>
+        </div>
+
+        <div className="div-button">
+          <button >Reset</button>
+          <button>Convert</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
