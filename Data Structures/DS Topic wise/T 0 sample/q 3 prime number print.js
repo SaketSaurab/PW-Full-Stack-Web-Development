7bad6c902c59dc2e0f3abab6e29d95df31149ecc
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-13 23:09:15 +0530

    Prime number

--- Data Structures/DS Topic wise/T 0 sample/q 3 prime number print.js
+++ Data Structures/DS Topic wise/T 0 sample/q 3 prime number print.js
@@ -1,28 +1,38 @@
-const readline=require("readline");
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-function primeNumberPrint(num){
-    if(num<2){
-        console.log("Enter number greater than 1");
-        return
+function primeCheck(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
     }
+  }
+  return true;
 }
 
-
-
-
-
-rl.question("Enter number : ",(input1)=>{
-    if(/^\d+$/.test(input1.trim())){
-let a=parseInt(input1.trim());
-
+function primeUptoN(num) {
+  let totalPrimes = 0;
+  for (let i = 2; i <= num; i++) {
+    if (primeCheck(i)) {
+      console.log(i);
+      totalPrimes++;
     }
-else{
+  }
+  console.log("Total prime numbers are : " + totalPrimes);
+}
+
+rl.question("Enter number : ", (input1) => {
+  if (/^\d+$/.test(input1.trim())) {
+    let a = parseInt(input1.trim());
+    primeUptoN(a);
+  } else {
     console.log("Enter valid number");
-    rl.close();
-
-}
-    } );
+  }
+  rl.close();
+});
