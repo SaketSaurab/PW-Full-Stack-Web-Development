55cbeb41888265602f1d660b3e9247af759c6938
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-16 15:44:56 +0530

    sample check

---
+++ Data Structures/DS Topic wise/T 0 sample/q 5 math.js
@@ -0,0 +1,35 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+function sum(a, b) {
+  console.log(`Addition of two number ${a} and ${b}`);
+  return console.log(`${a}+${b}=${a + b}`);
+}
+
+function sub(a, b) {
+  console.log(`Subtraction of two number ${a} and ${b}`);
+  return console.log(`${a} - ${b} = ${a - b}`);
+}
+
+function multi(a, b) {
+  console.log(`Multiplication of two number ${a} and ${b}`);
+  return(console.log(`${a} x ${b} = ${a*b}`))
+}
+
+rl.question("Enter first number ", (input1) => {
+  rl.question("Enter secon number ", (input2) => {
+    if (/^\d+$/.test(input1.trim()) && /^\d+$/.test(input2.trim())) {
+      let a = parseInt(input1.trim());
+      let b = parseInt(input2.trim());
+      sum(a, b);
+      sub(a,b);
+      multi(a,b);
+    } else {
+      console.log("Enter valid input");
+      rl.close();
+    }
+    rl.close();
+  });
+});
