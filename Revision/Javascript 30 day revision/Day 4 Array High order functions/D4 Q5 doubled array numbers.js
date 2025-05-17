072c011bcb792c74a73b7f54f26baa013aab6b6e
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-17 12:38:37 +0530

    react project list

--- Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q5 doubled array numbers.js
+++
@@ -1,6 +0,0 @@
-// Given an array of numbers, return an array where each number is doubled using `.map()`.
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
