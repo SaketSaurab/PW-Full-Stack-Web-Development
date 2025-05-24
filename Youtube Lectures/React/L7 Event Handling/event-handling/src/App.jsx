1e4f9047f0617109898db88cc958a6c50add103a
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-24 20:23:18 +0530

    event handling on form

--- Youtube Lectures/React/L7 Event Handling/event-handling/src/App.jsx
+++ Youtube Lectures/React/L7 Event Handling/event-handling/src/App.jsx
@@ -4,9 +4,34 @@ function App() {
   function handleClick() {
     alert("You clicked on button");
   }
+  function handleMouseOver() {
+    alert("Para ke upar mouse lekar aaye ho");
+  }
+
+  function handleInputChange(e) {
+    console.log("Input value change to : ", e.target.value);
+  }
+
+  function handleSubmit() {
+    alert("Form submit kar diya");
+  }
   return (
     <div className="container-div">
-      <button onClick={handleClick}> click me</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="enter the text"
+          type="text"
+          onChange={handleInputChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {/* <p
+        onMouseOver={handleMouseOver}
+        style={{ border: "1px solid black", padding: "5px", color: "Red" }}
+      >
+        This is paragraph text.
+      </p>
+      <button onClick={handleClick}> click me</button> */}
     </div>
   );
 }
