968fc4a4e1049a8d694b5750b2ee8b4f0c2abc05
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-26 19:22:53 +0530

    variation 3 : runs on each count update, variation 4: runs on each count/tatal updated ie. runs on multiple dependencies update

--- Youtube Lectures/React/L8 useEffect Hook/learn-useEffect/src/App.jsx
+++ Youtube Lectures/React/L8 useEffect Hook/learn-useEffect/src/App.jsx
@@ -3,6 +3,17 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const [total, setTotal] = useState(1);
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  function handleTotalClick() {
+    setTotal(total + 1);
+  }
+
   // *first =>side effect function
   // *secont=>cleanup fucntion
   // * third => comma seprated dependancy list
@@ -16,18 +27,35 @@ function App() {
 
   // variation 2 : run only on first render
 
+  // useEffect(() => {
+  //   console.log("I will run on first render only");
+  // }, []);
+
+  // variation 3: runs on
+
+  // useEffect(() => {
+  //   console.log("I will run when count is updated");
+  // }, [count]);
+
+  // variation 4: mutliple dependencies
+
   useEffect(() => {
-    console.log("I will run on first render only");
-  }, [3]);
-  const [count, setCount] = useState(0);
-  function handleClick() {
-    setCount(count + 1);
-  }
+    console.log("count/total is updated");
+  }, [count, total]);
+
+  //
+
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>Count: {count}</h1>
       <br />
       <button onClick={handleClick}>Click me</button>
+
+      <br />
+      <br />
+      <h1>total: {total}</h1>
+      <br />
+      <button onClick={handleTotalClick}>Click me</button>
     </div>
   );
 }
