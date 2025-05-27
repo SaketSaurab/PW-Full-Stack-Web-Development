5526ea68266531ee3f1b899052dffeebcb2aa7fa
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-27 19:40:53 +0530

    Logger component

--- Youtube Lectures/React/L8 useEffect Hook/learn-useEffect/src/App.jsx
+++ Youtube Lectures/React/L8 useEffect Hook/learn-useEffect/src/App.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 
 import "./App.css";
+import LoggerComponent from "./components/LoggerComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const [total, setTotal] = useState(1);
-  function handleClick() {
-    setCount(count + 1);
-  }
+  // const [total, setTotal] = useState(1);
+  // function handleClick() {
+  //   setCount(count + 1);
+  // }
 
-  function handleTotalClick() {
-    setTotal(total + 1);
-  }
+  // function handleTotalClick() {
+  //   setTotal(total + 1);
+  // }
 
   // *first =>side effect function
   // *secont=>cleanup fucntion
   // * third => comma seprated dependancy list
+
+  //----------------------------------------------------
 
   // variation 1 : runs on every render
 
@@ -25,29 +28,44 @@ function App() {
   //   console.log("I will run on each render");
   // });
 
+  //----------------------------------------------------
+
   // variation 2 : run only on first render
 
   // useEffect(() => {
   //   console.log("I will run on first render only");
   // }, []);
 
-  // variation 3: runs on
+  //----------------------------------------------------
+
+  // variation 3: runs on each count update
 
   // useEffect(() => {
   //   console.log("I will run when count is updated");
   // }, [count]);
 
+  //----------------------------------------------------
+
   // variation 4: mutliple dependencies
 
-  useEffect(() => {
-    console.log("count/total is updated");
-  }, [count, total]);
+  // useEffect(() => {
+  //   console.log("count/total is updated");
+  // }, [count, total]);
 
+  //----------------------------------------------------
+  // variation 5
+
+  // useEffect(() => {
+  //   console.log("Count/total is updated");
+  //   return () => {
+  //     console.log("count/total is unmounted");
+  //   };
+  // }, [count, total]);
   //
 
   return (
     <div>
-      <h1>Count: {count}</h1>
+      {/* <h1>Count: {count}</h1>
       <br />
       <button onClick={handleClick}>Click me</button>
 
@@ -55,7 +73,8 @@ function App() {
       <br />
       <h1>total: {total}</h1>
       <br />
-      <button onClick={handleTotalClick}>Click me</button>
+      <button onClick={handleTotalClick}>Click me</button> */}
+      <LoggerComponent />
     </div>
   );
 }
