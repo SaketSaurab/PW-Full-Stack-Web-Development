9f9148bc04ee1c20a5f1a1114605749a79e7f81f
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-30 16:37:06 +0530

    UI updated

--- Youtube Lectures/React/L 10 React Routing/react-router-application/src/components/Navbar.jsx
+++ Youtube Lectures/React/L 10 React Routing/react-router-application/src/components/Navbar.jsx
@@ -3,19 +3,34 @@ import Home from "./Home";
 import Dashboard from "./Dashboard";
 import About from "./About";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link to="/dashboard">Dashboard</Link>
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Dashboard
+          </NavLink>
         </li>
         <li>
-          <Link to="/about">about</Link>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            about
+          </NavLink>
         </li>
       </ul>
     </div>
