849622635c5cf59ff0d60ff32d4beca1ad0dd445
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-30 16:53:08 +0530

    useParam hook used

--- Youtube Lectures/React/L 10 React Routing/react-router-application/src/App.jsx
+++ Youtube Lectures/React/L 10 React Routing/react-router-application/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
+import ParamComp from "./components/ParamComp";
 
 const router = createBrowserRouter([
   {
@@ -30,6 +31,15 @@ const router = createBrowserRouter([
       <div>
         <Navbar />
         <Dashboard />
+      </div>
+    ),
+  },
+  {
+    path: "/student/:id",
+    element: (
+      <div>
+        <Navbar />
+        <ParamComp />
       </div>
     ),
   },
