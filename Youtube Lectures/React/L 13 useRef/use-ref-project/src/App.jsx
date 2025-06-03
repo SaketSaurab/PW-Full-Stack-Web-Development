11375e43fee63d90e1cac39b52b2b65ae27ae773
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-06-03 20:13:47 +0530

    Changing the button color using useRef

--- Youtube Lectures/React/L 13 useRef/use-ref-project/src/App.jsx
+++ Youtube Lectures/React/L 13 useRef/use-ref-project/src/App.jsx
@@ -5,13 +5,17 @@ function App() {
   const [count, setCount] = useState(0);
 
   let value = useRef(0);
+  let btnRef = useRef();
   function handleIncrement() {
     value.current = value.current + 1;
     setCount(count + 1);
-    console.log("Value updated to", value);
+    console.log("Value updated to", value.current);
   }
   function handleDecrement() {
     setCount(count - 1);
+  }
+  function handleColourChange() {
+    btnRef.current.style.backgroundColor = "red";
   }
   useEffect(() => {
     console.log("mai firse render ho gaya hu");
@@ -20,10 +24,15 @@ function App() {
     <div>
       <div>Count: {count}</div>
       <div>
-        <button onClick={handleIncrement}>Increment</button>
+        <button ref={btnRef} onClick={handleIncrement}>
+          Increment
+        </button>
       </div>
       <div>
         <button onClick={handleDecrement}>Decrement</button>
+      </div>
+      <div>
+        <button onClick={handleColourChange}>Change colour of button</button>
       </div>
     </div>
   );
