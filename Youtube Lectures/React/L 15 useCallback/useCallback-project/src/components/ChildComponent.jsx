300da4373ebd9f3f283b932070e2f4541edf0005
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-06-06 16:34:10 +0530

    after using using useMemo the child component will not re-render untill the data is changed

--- Youtube Lectures/React/L 15 useCallback/useCallback-project/src/components/ChildComponent.jsx
+++ Youtube Lectures/React/L 15 useCallback/useCallback-project/src/components/ChildComponent.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useMemo } from "react";
 
-const ChildComponent = (props) => {
+const ChildComponent = useMemo((props) => {
   console.log("Child component got re-rendered");
   return (
     <div>
       <button>{props.buttonName}</button>
     </div>
   );
-};
+});
 
 export default ChildComponent;
