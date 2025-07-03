3a8b4a01474c07433db44b43e601edc35b9ff9ce
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-07-03 16:02:10 +0530

    stop watch app using useRef

--- React/react-vite-13/stop-watch/src/App.jsx
+++ React/react-vite-13/stop-watch/src/App.jsx
@@ -1,36 +1,36 @@
-
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  const[time,setTime]=useState(0);
-  function startTimer(){
-
+  const [time, setTime] = useState(0);
+  let timeRef = useRef(null);
+  function startTimer() {
+    timeRef.current = setInterval(() => {
+      setTime(time=>time + 1);
+    }, 1000);
   }
-  function stopTimer(){
-
+  function stopTimer() {
+    clearInterval(timeRef.current);
+    timeRef.current = null;
   }
-  function resetTimer(){
-
+  function resetTimer() {
+    stopTimer();
+    setTime(0);
   }
-
 
   return (
-   <div>
-   <h1>Stop Watch : {time} seconds</h1>
-   <button onClick={startTimer}>
-Start Timer
-   </button>
-   <br/><br/>
-   <button onClick={stopTimer}>
-    Stop Timer
-   </button>
-   <br/><br/>
-   <button onClick={resetTimer}>
-    Reset Timer
-   </button>
-   </div>
-  )
+    <div>
+      <h1>Stop Watch : {time} seconds</h1>
+      <button onClick={startTimer}>Start Timer</button>
+      <br />
+      <br />
+      <button onClick={stopTimer}>Stop Timer</button>
+      <br />
+      <br />
+      <button onClick={resetTimer}>Reset Timer</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
