63f2f95089508d66f675586771a8a80f917446c5
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-09-12 13:42:28 +0530

    Topics and notes

---
+++ Data Structures/DS Topic wise/T 0 sample/q n temp.js
@@ -0,0 +1,23 @@
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+
+// function number(a){
+//     for(let i=1;i<=a;i++){
+//         console.log(`${i}`)
+//     }
+// }
+
+// rl.question("Enter number to print ",(input1)=>{
+//     if(/^\d+$/.test(input1.trim())){
+//         let a=parseInt(input1.trim());
+//         number(a);
+//     }
+//     else{
+//         console.log("Enter valid number ");
+//     }
+//     rl.close();
+// })
