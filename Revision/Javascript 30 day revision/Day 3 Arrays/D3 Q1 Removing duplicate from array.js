3c2a384db0e212ff2e7abc1a36b1cf5129d621ae
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-10-05 22:24:43 +0530

    js code to remove duplicate elements

--- Revision/Javascript 30 day revision/Day 3 Arrays/D3 Q1 Removing duplicate from array.js
+++ Revision/Javascript 30 day revision/Day 3 Arrays/D3 Q1 Removing duplicate from array.js
@@ -1,2 +1,27 @@
 // Remove duplicates from an array without using Set.
 
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function removeDuplicate(input) {
+  let arr = input.split(/\s+/);
+  //   console.log(arr);
+  let result = [];
+  let check = {};
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+    if (!check[item]) {
+      check[item] = true;
+      result.push(item);
+    }
+  }
+  console.log(`Unique elements : ${result}`);
+}
+
+rl.question("The the array elements : ", (input1) => {
+  removeDuplicate(input1.trim());
+  rl.close();
+});
