383c51fd0aea333ae7eb67590793929839e19e02
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-10-08 11:25:22 +0530

    reduce function

--- Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q2 Implement filter function.js
+++ Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q2 Implement filter function.js
@@ -1,28 +1,25 @@
 // Implement your own `filter()` function.
-const readline=require('readline');
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-function evenNumber(num){
- let  arr1=num.filter(a=>a%2===0);
+function evenNumber(num) {
+  let arr1 = num.filter((a) => a % 2 === 0);
   console.log(arr1);
-
 }
-function askInput(){
-    
-rl.question("Enter the number : ",(input)=>{
-    let arr1=input.trim().split(" ");
-    let arr2=arr1.every((val)=>val !=="" && !isNaN(val));
-    if(!arr2){
-        console.log("Enter number only ");
-        return askInput();
+function askInput() {
+  rl.question("Enter the number : ", (input) => {
+    let arr1 = input.trim().split(" ");
+    let arr2 = arr1.every((val) => val !== "" && !isNaN(val));
+    if (!arr2) {
+      console.log("Enter number only ");
+      return askInput();
+    } else {
+      let numArr = arr1.map(Number);
+      evenNumber(numArr);
     }
-    else{
-       let numArr=arr1.map(Number);
-       evenNumber(numArr);
-    }
-})
+  });
 }
 askInput();
---
+++ Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q3 Implementing reduce function.js
@@ -0,0 +1,30 @@
+// Implement your own `reduce()` function.
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function red(input) {
+  let sum = input.reduce((total, num) => {
+    return total + num;
+  }, 0);
+
+  console.log(`The sum of the elements ${sum}`);
+}
+
+function askInput() {
+  rl.question("Enter the array input numbers : ", (input1) => {
+    let arr1 = input1.trim().split(" ");
+    let arr2 = arr1.every((val) => val !== "" && !isNaN(val));
+    if (!arr2) {
+      console.log("Enter numbers only ");
+      return askInput();
+    } else {
+      let numArr = arr1.map(Number);
+      red(numArr);
+    }
+  });
+}
+
+askInput();
