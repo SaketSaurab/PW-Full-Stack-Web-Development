d2b45dd8d89babcc76836cddc98b6c5a7220526d
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-10-09 12:48:50 +0530

    double the array elements

---
+++ Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q5 array with doubled number.js
@@ -0,0 +1,6 @@
+// Given an array of numbers, return an array where each number is doubled using `.map()`.
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
