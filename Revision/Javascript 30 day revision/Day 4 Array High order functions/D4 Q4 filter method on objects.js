1fa162d228ee278fc120d86dc8f9f4e2a190603e
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-10-09 13:20:14 +0530

    filter method on object

--- Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q4 filter method on objects.js
+++ Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q4 filter method on objects.js
@@ -1,1 +1,34 @@
 // Given an array of objects `{name, age}`, return only names of people older than 18.
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const people = [];
+let total;
+rl.question("Enter the total number of people : ", (input1) => {
+  total = parseInt(input1.trim());
+
+  takeInput();
+});
+let count = 0;
+function takeInput() {
+  if (count < total) {
+    rl.question("Enter name : ", (name) => {
+      rl.question("Enter age : ", (age) => {
+        people.push({
+          name: name,
+          age: Number(age),
+        });
+        count++;
+        takeInput();
+      });
+    });
+  } else {
+    const result = people
+      .filter((person) => person.age > 18)
+      .map((person) => person.name);
+
+    console.log(result);
+  }
+}
