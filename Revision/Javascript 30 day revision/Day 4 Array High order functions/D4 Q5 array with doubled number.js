f76be358f11df75fc78cf2ad669d12459ed4ea0b
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-10-09 19:58:56 +0530

    map filter to double the array input

--- Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q5 array with doubled number.js
+++ Revision/Javascript 30 day revision/Day 4 Array High order functions/D4 Q5 array with doubled number.js
@@ -4,3 +4,23 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+function doubleTheNum(num) {
+  let arr1 = num.map((a) => a * 2);
+  console.log(arr1);
+}
+
+function askInput() {
+  rl.question("Enter the array elements ", (input1) => {
+    let arr1 = input1.trim().split(" ");
+    let arr2 = arr1.every((a) => a !== "" && !isNaN(a));
+    if (!arr2) {
+      console.log("Enter the number only : ");
+      return askInput();
+    } else {
+      let numArr = arr1.map(Number);
+      doubleTheNum(numArr);
+    }
+  });
+}
+askInput();
