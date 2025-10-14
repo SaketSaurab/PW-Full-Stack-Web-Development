91becd6a39f1211df57d6d409fead9e9e1e2adb9
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-10-14 23:03:05 +0530

    note app checked

--- Revision/React1/sample-app/src/App.jsx
+++ Revision/React1/sample-app/src/App.jsx
@@ -1,7 +1,44 @@
 import { useState } from "react";
+import "./app.css";
 
 function App() {
-  return <>Hello World</>;
+  const [note, setNote] = useState("");
+  const [data, setData] = useState([]);
+
+  const addData = () => {
+    if (note.trim() === "") return;
+    setData([...data, note]);
+    setNote("");
+  };
+  const deleteNote = (index) => {
+    const updatedNote = data.filter((_, i) => i !== index);
+    setData(updatedNote);
+  };
+  return (
+    <>
+      <div>
+        <div className="input-div">
+          <input
+            type="text"
+            placeholder="Enter text here"
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+          />
+          <button onClick={addData}>Add text</button>
+        </div>
+        <div className="output-div">
+          <ul>
+            {data.map((item, index) => (
+              <li key={index}>
+                {item}
+                <button onClick={() => deleteNote(index)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
