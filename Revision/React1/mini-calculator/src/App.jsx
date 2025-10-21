875382c4d653a2be691e4123951be8ca8cf7f5d5
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-10-21 21:59:55 +0530

    mini calculator

---
+++ Revision/React1/mini-calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="container-div">
+        <div className="title">Calculator</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
