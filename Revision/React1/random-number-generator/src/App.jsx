be9bd994074c2e1e8815b338ec96d556762f9818
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-10-25 19:18:16 +0530

    UI and animation added

--- Revision/React1/random-number-generator/src/App.jsx
+++ Revision/React1/random-number-generator/src/App.jsx
@@ -3,24 +3,45 @@ import "./App.css";
 
 function App() {
   const [num, setNum] = useState("");
+  const [len, setLen] = useState("1");
+  const [animate, setAnimate] = useState(false);
 
   const oneDigit = () => {
-    setNum(Math.floor(Math.random() * 10));
+    const length = parseInt(len);
+    const max = 10 ** length;
+    setAnimate(false);
+    setTimeout(() => {
+      setNum(Math.floor(Math.random() * max));
+      setAnimate(true);
+    }, 10);
   };
-  const twodigit = () => {
-    setNum(Math.floor(Math.random() * 100));
-  };
-  const threeDigit = () => {
-    setNum(Math.floor(Math.random() * 1000));
-  };
+  // const twodigit = () => {
+  //   setNum(Math.floor(Math.random() * 100));
+  // };
+  // const threeDigit = () => {
+  //   setNum(Math.floor(Math.random() * 1000));
+  // };
+
   return (
     <>
       <div className="container-div">
-        <div className="title-div">Random Number Generator</div>
-        <div className="result-div">{num}</div>
-        <button onClick={oneDigit}>One digit number</button>
-        <button onClick={twodigit}>Two digit number</button>
-        <button onClick={threeDigit}>Three Digit number</button>
+        <div className="card">
+          <h1 className="title"> Random Number Generator</h1>
+          <p className="sub-title"> Generate numbers of any length</p>
+          <div className={`result-box ${animate ? "pop" : ""}`}>
+            {num || "—"}
+          </div>
+          <label className="label">Number Length</label>
+          <input
+            type="number"
+            value={len}
+            onChange={(e) => setLen(e.target.value)}
+            placeholder="Lenght of digit to generate"
+          />
+          <button onClick={oneDigit}>Generate Number</button>
+          {/* <button onClick={twodigit}>Two digit number</button>
+        <button onClick={threeDigit}>Three Digit number</button> */}
+        </div>{" "}
       </div>
     </>
   );
