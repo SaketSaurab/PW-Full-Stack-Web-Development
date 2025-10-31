62bd4f310327a63a9ed7fe8029f848a8d3872333
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-11-01 00:07:09 +0530

    small program to calculate the area and perimeter of rectangle and square

---
+++ Revision/React1/square-area-calculator/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState("");
+  const [breadth, setBreadth] = useState("");
+  const [area, setArea] = useState(null);
+  const [perimeter, setPerimeter] = useState(null);
+
+  const handleCalculate = () => {
+    const l = parseFloat(length);
+    const b = parseFloat(breadth);
+
+    if (isNaN(l) || isNaN(b) || l <= 0 || b <= 0) {
+      alert("Enter valid positive number");
+      return;
+    }
+    setArea(l * b);
+    setPerimeter(2 * (l + b));
+  };
+  const handleReset = () => {
+    setLength("");
+    setBreadth("");
+    setArea(null);
+    setPerimeter(null);
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <h2> Calculate area and perimeter</h2>
+
+          <input
+            type="number"
+            className="input-filed"
+            value={length}
+            placeholder="Enter the length "
+            onChange={(e) => setLength(e.target.value)}
+          />
+
+          <input
+            type="number"
+            className="input-filed"
+            value={breadth}
+            placeholder="Enter the width "
+            onChange={(e) => setBreadth(e.target.value)}
+          />
+          <button className="button" onClick={handleCalculate}>
+            Calculate
+          </button>
+          <button onClick={handleReset}>Reset</button>
+          {area != null && perimeter != null && (
+            <div className="result-box">
+              <p>Area : {area}</p>
+              <p>Perimeter : {perimeter}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
