235502f16ed5bc9168505df2b1d56474bc9d669c
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-11-10 17:56:38 +0530

    redux initial

---
+++ Practice/Redux/redux-intro-1/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>Navbar hai ye</div>;
+};
+export const HelloBro = ({ count1 }) => {
+  return <div>Hello bro count:{count1}</div>;
+};
+export default Navbar;
